e554e3c33bce983dcc54618959082e3cf639b0f8
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-06-17 19:01:07 +0530

    Update GSP421.sh

--- APIs Explorer Cloud Storage/GSP421.sh
+++ APIs Explorer Cloud Storage/GSP421.sh
@@ -1,17 +1,17 @@
-gcloud alpha services api-keys create --display-name="AbhiArcade"  
-KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=AbhiArcade") 
+gcloud alpha services api-keys create --display-name="Abhi"  
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=Abhi") 
 export API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)") 
 echo $API_KEY
 
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 
-gsutil mb gs://$DEVSHELL_PROJECT_ID-AbhiArcade
+gsutil mb gs://$DEVSHELL_PROJECT_ID-abhi
 
 gsutil cp demo-image1.png gs://$DEVSHELL_PROJECT_ID
 
 gsutil cp demo-image2.png gs://$DEVSHELL_PROJECT_ID
 
-gsutil cp demo-image1-copy.png gs://$DEVSHELL_PROJECT_ID-AbhiArcade
+gsutil cp demo-image1%20-%20Copy.png gs://$DEVSHELL_PROJECT_ID-abhi
 
 curl -LO https://github.com/gcpsolution99/GCP-solution/blob/main/APIs%20Explorer%20Cloud%20Storage/demo-image1%20-%20Copy.png
 curl -LO https://github.com/gcpsolution99/GCP-solution/blob/main/APIs%20Explorer%20Cloud%20Storage/demo-image1.png
