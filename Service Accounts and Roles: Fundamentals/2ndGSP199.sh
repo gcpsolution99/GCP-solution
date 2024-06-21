bddb789ce8b5c7fc40f483d2e57d4ffcf7c6086f
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-06-21 22:05:57 +0530

    Create 2ndGSP199.sh

---
+++ Service Accounts and Roles: Fundamentals/2ndGSP199.sh
@@ -0,0 +1,35 @@
+export PROJECT_ID=$(gcloud config get-value project)
+
+#  Install the necessary dependencies by running the following commands
+sudo apt-get update
+sudo apt-get install -y git python3-pip
+pip3 install --upgrade pip
+pip3 install google-cloud-bigquery
+pip3 install pyarrow
+pip3 install pandas
+pip3 install db-dtypes
+
+# create the example Python file
+echo "
+from google.auth import compute_engine
+from google.cloud import bigquery
+credentials = compute_engine.Credentials(
+    service_account_email='bigquery-qwiklab@$PROJECT_ID.iam.gserviceaccount.com')
+query = '''
+SELECT
+  year,
+  COUNT(1) as num_babies
+FROM
+  publicdata.samples.natality
+WHERE
+  year > 2000
+GROUP BY
+  year
+'''
+client = bigquery.Client(
+    project='$PROJECT_ID',
+    credentials=credentials)
+print(client.query(query).to_dataframe())
+" > query.py
+
+python3 query.py
