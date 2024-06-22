e478205303d7aa4181d9486172ef592a6d055ee1
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-06-22 11:49:01 +0530

    Create GSP073.sh

---
+++ Cloud Storage: Qwik Start - Cloud Console/GSP073.sh
@@ -0,0 +1,9 @@
+export PROJECT_ID=$(gcloud config get-value project)
+
+gsutil mb -l $REGION -c Standard gs://$PROJECT_ID
+
+curl -O https://github.com/gcpsolution99/GCP-solution/blob/main/kitten.png
+
+gsutil cp kitten.png gs://$PROJECT_ID/kitten.png
+
+gsutil iam ch allUsers:objectViewer gs://$PROJECT_ID
