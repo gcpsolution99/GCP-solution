fbf03680865984a3fdf600ac498702bc1a33207d
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-06-24 17:25:40 +0530

    Update GSP277.sh

--- APIs Explorer: Qwik Start/GSP277.sh
+++ APIs Explorer: Qwik Start/GSP277.sh
@@ -1,5 +1,5 @@
 export PROJECT_ID=$(gcloud config get-value project)
 gsutil mb -p $PROJECT_ID -c regional -l us-east1 gs://$PROJECT_ID-bucket
-curl -L -o demo-image.jpg https://github.com/
+curl -L -o demo-image.jpg https://raw.githubusercontent.com/gcpsolution99/GCP-solution/main/APIs%20Explorer%3A%20Qwik%20Start/demo-image.jpg
 gsutil cp demo-image.jpg gs://$PROJECT_ID-bucket/demo-image.jpg
 gsutil acl ch -u allUsers:R gs://$PROJECT_ID-bucket/demo-image.jpg
