301b9f9e0241d3722a1ff7d58b027e2d9194829e
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-07-03 20:50:18 +0530

    Create 1stARC112.sh

---
+++ App Engine: 3 Ways: Challenge Lab/1stARC112.sh
@@ -0,0 +1,4 @@
+gcloud services enable appengine.googleapis.com
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+cd python-docs-samples/appengine/standard_python3/hello_world
+exit
