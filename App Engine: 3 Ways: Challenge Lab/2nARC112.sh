a746a9563a4f360785284b552d4c25eb1c8408ca
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-07-23 12:27:27 +0530

    Delete App Engine: 3 Ways: Challenge Lab/2nARC112.sh

--- App Engine: 3 Ways: Challenge Lab/2nARC112.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
-cd python-docs-samples/appengine/standard_python3/hello_world
-gcloud app deploy
-sed -i 's/Hello World!/'"$MESSAGE"'/g' main.py
-gcloud app deploy
