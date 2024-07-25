719944bc8fdc3a2df7ebb4dd2a1c44f11bbc3e51
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-07-25 20:20:23 +0530

    Create ARC120.sh

---
+++ ARC/ARC120.sh
@@ -0,0 +1,23 @@
+gcloud compute instances create my-instance \
+    --machine-type=e2-medium \
+    --zone=$ZONE \
+    --image-project=debian-cloud \
+    --image-family=debian-11 \
+    --boot-disk-size=10GB \
+    --boot-disk-type=pd-balanced \
+    --create-disk=size=100GB,type=pd-standard,mode=rw,device-name=additional-disk \
+    --tags=http-server
+gcloud compute disks create mydisk \
+    --size=200GB \
+    --zone=$ZONE
+gcloud compute instances attach-disk my-instance \
+    --disk=mydisk \
+    --zone=$ZONE
+gcloud compute ssh my-instance --zone=$ZONE --quiet
+
+
+sleep 5
+
+sudo apt update
+sudo apt install nginx -y
+sudo systemctl start nginx
