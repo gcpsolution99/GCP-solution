31c11f3f3d59a761b0b6a1cd04cca3e0cabb3171
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-07-25 21:12:17 +0530

    Update ARC125second.sh

--- ARC/ARC125second.sh
+++ ARC/ARC125second.sh
@@ -1,3 +1,31 @@
+gcloud services enable fitness.googleapis.com
+export OAUTH2_TOKEN=$(gcloud auth print-access-token)
+echo '{
+  "name": "'"$DEVSHELL_PROJECT_ID"'-bucket-1",
+  "location": "us",
+  "storageClass": "multi_regional"
+}
+' > values.json
+curl -X POST --data-binary @values.json \
+    -H "Authorization: Bearer $OAUTH2_TOKEN" \
+    -H "Content-Type: application/json" \
+    "https://www.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
+echo '{
+  "name": "'"$DEVSHELL_PROJECT_ID"'-bucket-2",
+  "location": "us",
+  "storageClass": "multi_regional"
+}' > values.json
+curl -X POST --data-binary @values.json \
+    -H "Authorization: Bearer $OAUTH2_TOKEN" \
+    -H "Content-Type: application/json" \
+    "https://www.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
+    
+
+
+
+sleep 10
+    
+    
     curl -X POST --data-binary @/home/gcpstaging25084_student/demo-image.png \
     -H "Authorization: Bearer $OAUTH2_TOKEN" \
     -H "Content-Type: image/png" \
