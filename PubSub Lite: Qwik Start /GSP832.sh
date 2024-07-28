34f7e11303ff4e66a96ec59c45940a124fa6dfe9
GCP Arcade Tech <69536662+gcpsolution99@users.noreply.github.com>
2024-07-28 19:30:48 +0530

    Update GSP832.sh

--- PubSub Lite: Qwik Start /GSP832.sh
+++ PubSub Lite: Qwik Start /GSP832.sh
@@ -5,12 +5,12 @@ sleep 30
 pip3 install --upgrade google-cloud-pubsublite
 
 gcloud pubsub lite-topics create my-lite-topic \
-          --location=$REGION-a --partitions=1 \
-          --per-partition-bytes=30GiB --message-retention-period=2w
+--location=$REGION-c --partitions=1 \
+--per-partition-bytes=30GiB --message-retention-period=2w
 
 
 gcloud pubsub lite-subscriptions create my-lite-subscription \
-  --project=$DEVSHELL_PROJECT_ID \
-  --location=$REGION-a \
-  --topic=my-lite-topic \
-  --delivery-requirement=deliver-after-stored
+--project=$DEVSHELL_PROJECT_ID \
+--location=$REGION-c \
+--topic=my-lite-topic \
+--delivery-requirement=deliver-after-stored
