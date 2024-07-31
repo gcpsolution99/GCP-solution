28bdab37d0e85b4b7f1e0b303b0781a9be4df623
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-07-31 14:45:39 +0530

    Update GSP416.sh

--- GSP/GSP416.sh
+++ GSP/GSP416.sh
@@ -1,16 +1,53 @@
+
 bq mk fruit_store
 
-bq mk --table --description "Table for fruit details" $DEVSHELL_PROJECT_ID:fruit_store.fruit_details
+bq query --use_legacy_sql=false '
+SELECT
+["raspberry", "blackberry", "strawberry", "cherry"] AS fruit_array
+'
+bq query --use_legacy_sql=false '
+SELECT person, fruit_array, total_cost FROM `data-to-insights.advanced.fruit_store`;
+'
 
-bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect $DEVSHELL_PROJECT_ID:fruit_store.fruit_details gs://data-insights-course/labs/optimizing-for-performance/shopping_cart.json
+bq query --use_legacy_sql=false \
+"
+SELECT
+  fullVisitorId,
+  date,
+  v2ProductName,
+  pageTitle
+  FROM \`data-to-insights.ecommerce.all_sessions\`
+WHERE visitId = 1501570398
+ORDER BY date
+"
 
-echo "${GREEN}${BOLD}
+bq query --use_legacy_sql=false \
+"
+SELECT
+  fullVisitorId,
+  date,
+  ARRAY_AGG(v2ProductName) AS products_viewed,
+  ARRAY_AGG(pageTitle) AS pages_viewed
+  FROM \`data-to-insights.ecommerce.all_sessions\`
+WHERE visitId = 1501570398
+GROUP BY fullVisitorId, date
+ORDER BY date
+"
 
-Task 2 Completed
-
-${RESET}"
-
-#task 3
+bq query --use_legacy_sql=false \
+"
+SELECT
+  fullVisitorId,
+  date,
+  ARRAY_AGG(v2ProductName) AS products_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(v2ProductName)) AS num_products_viewed,
+  ARRAY_AGG(pageTitle) AS pages_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(pageTitle)) AS num_pages_viewed
+  FROM \`data-to-insights.ecommerce.all_sessions\`
+WHERE visitId = 1501570398
+GROUP BY fullVisitorId, date
+ORDER BY date
+"
 
 bq query --use_legacy_sql=false \
 "
@@ -27,15 +64,13 @@ GROUP BY fullVisitorId, date
 ORDER BY date
 "
 
-
-echo "${GREEN}${BOLD}
-
-Task 3 Completed
-
-${RESET}"
-
-#task 4
-
+bq query --use_legacy_sql=false \
+"
+SELECT
+  *
+FROM \`bigquery-public-data.google_analytics_sample.ga_sessions_20170801\`
+WHERE visitId = 1501570398
+"
 
 bq query --use_legacy_sql=false \
 "
@@ -48,16 +83,33 @@ WHERE visitId = 1501570398
 LIMIT 10
 "
 
-echo "${GREEN}${BOLD}
+bq query --use_legacy_sql=false \
+"
+SELECT
+  visitId,
+  totals.*,
+  device.*
+FROM \`bigquery-public-data.google_analytics_sample.ga_sessions_20170801\`
+WHERE visitId = 1501570398
+LIMIT 10
+"
 
-Task 4 Completed
+bq query --use_legacy_sql=false '
+#standardSQL
+SELECT STRUCT("Rudisha" as name, 23.4 as split) as runner
+'
 
-${RESET}"
+bq query --use_legacy_sql=false '
+#standardSQL
+SELECT STRUCT("Rudisha" as name, [23.4, 26.3, 26.4, 26.1] as splits) AS runner
+'
 
-#task 6
+bq mk racing
 
-# Define the schema in a JSON file, e.g., schema.json
-echo '[
+bq mk --table $DEVSHELL_PROJECT_ID:fruit_store.fruit_details
+bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect $DEVSHELL_PROJECT_ID:fruit_store.fruit_details gs://data-insights-course/labs/optimizing-for-performance/shopping_cart.json
+cat <<EOF_END > c.json
+[
     {
         "name": "race",
         "type": "STRING",
@@ -80,64 +132,47 @@ echo '[
             }
         ]
     }
-]' > schema.json
+]
+EOF_END
 
+bq mk --table --schema=c.json $DEVSHELL_PROJECT_ID:racing.race_results 
+bq load --source_format=NEWLINE_DELIMITED_JSON --schema=c.json $DEVSHELL_PROJECT_ID:racing.race_results gs://data-insights-course/labs/optimizing-for-performance/race_results.json
 
-bq mk racing
+bq query --use_legacy_sql=false '
+SELECT * FROM racing.race_results
+'
 
-bq mk --table --schema=schema.json --description "Table for race details" $DEVSHELL_PROJECT_ID:racing.race_results 
+bq query --use_legacy_sql=false '
+SELECT race, participants.name
+FROM racing.race_results
+CROSS JOIN
+race_results.participants 
+'
 
-bq load --source_format=NEWLINE_DELIMITED_JSON --schema=schema.json $DEVSHELL_PROJECT_ID:racing.race_results gs://data-insights-course/labs/optimizing-for-performance/race_results.json
+bq query --use_legacy_sql=false '
+SELECT race, participants.name
+FROM racing.race_results AS r, r.participants
+'
 
-echo "${GREEN}${BOLD}
-
-Task 6 Completed
-
-${RESET}"
-
-#task 7
-
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '
 SELECT COUNT(p.name) AS racer_count
 FROM racing.race_results AS r, UNNEST(r.participants) AS p
-"
+'
 
-echo "${GREEN}${BOLD}
-
-Task 7 Completed
-
-${RESET}"
-
-#task 8
-
-
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '
 SELECT
   p.name,
   SUM(split_times) as total_race_time
 FROM racing.race_results AS r
 , UNNEST(r.participants) AS p
 , UNNEST(p.splits) AS split_times
-WHERE p.name LIKE 'R%'
+WHERE p.name LIKE "R%"
 GROUP BY p.name
 ORDER BY total_race_time ASC;
-"
+'
 
-echo "${GREEN}${BOLD}
-
-Task 8 Completed
-
-${RESET}"
-
-#task 9
-
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '
+#standard
 SELECT
   p.name,
   split_time
@@ -145,10 +180,4 @@ FROM racing.race_results AS r
 , UNNEST(r.participants) AS p
 , UNNEST(p.splits) AS split_time
 WHERE split_time = 23.2;
-"
-
-echo "${GREEN}${BOLD}
-
-Task 9 Completed
-
-${RESET}"
+'
