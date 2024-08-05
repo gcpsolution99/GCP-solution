308c19be66d159cdd2393461d5a383e2a72bdd8d
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-08-05 23:04:23 +0530

    Update GSP087.sh

--- GSP/GSP087.sh
+++ GSP/GSP087.sh
@@ -2,24 +2,15 @@ gsutil mb gs://$DEVSHELL_PROJECT_ID
 
 gsutil cp -r gs://spls/gsp087/* gs://$DEVSHELL_PROJECT_ID
 
-echo "${GREEN}${BOLD}
 
 Task 2 Completed
 
-${RESET}"
-
 gcloud compute instance-templates create autoscaling-instance01 --metadata=startup-script-url=gs://$DEVSHELL_PROJECT_ID/startup.sh,gcs-bucket=gs://$DEVSHELL_PROJECT_ID
-
-
-echo "${GREEN}${BOLD}
 
 Task 3 Completed
 
-${RESET}"
-
 gcloud beta compute instance-groups managed create autoscaling-instance-group-1 --project=$DEVSHELL_PROJECT_ID --base-instance-name=autoscaling-instance-group-1 --size=1 --template=projects/$DEVSHELL_PROJECT_ID/global/instanceTemplates/autoscaling-instance01 --zone=$ZONE --list-managed-instances-results=PAGELESS --no-force-update-on-repair --default-action-on-vm-failure=repair && gcloud beta compute instance-groups managed set-autoscaling autoscaling-instance-group-1 --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --cool-down-period=60  --max-num-replicas=3 --min-num-replicas=1 --mode=on --target-cpu-utilization=0.6 --stackdriver-metric-filter=resource.type\ =\ \"gce_instance\" --update-stackdriver-metric=custom.googleapis.com/appdemo_queue_depth_01 --stackdriver-metric-utilization-target=150.0 --stackdriver-metric-utilization-target-type=gauge
 
-echo "${GREEN}${BOLD}
 
 Task 4 & 7  Completed
 
