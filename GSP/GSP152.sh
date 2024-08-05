f1ce1e99b731374c6e152c0338b5f4b55293e5d0
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-08-05 23:36:22 +0530

    Create GSP152.sh

---
+++ GSP/GSP152.sh
@@ -0,0 +1,6 @@
+gcloud sql instances create myinstance \
+    --database-version=POSTGRES_15 \
+    --tier=db-custom-2-7680 \
+    --region=$REGION \
+    --storage-type=SSD \
+    --storage-size=100GB
