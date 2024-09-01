e5fa570fc52e54436b4d4e49c3dd776f44e92efc
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-09-01 12:51:55 +0530

    Create GSP1166.sh

---
+++ GSP/GSP1166.sh
@@ -0,0 +1,13 @@
+echo "   https://console.cloud.google.com/vertex-ai/publishers/google/model-garden/text-bison?project=$DEVSHELL_PROJECT_ID"
+echo " "
+
+echo "-----------------------------------------------"
+echo " "
+
+echo "   https://console.cloud.google.com/vertex-ai/publishers/google/model-garden/owlvit-base-patch32?project=$DEVSHELL_PROJECT_ID"
+echo " "
+
+echo "-----------------------------------------------"
+echo " "
+
+echo "   https://console.cloud.google.com/vertex-ai/publishers/google/model-garden/bert-base?project=$DEVSHELL_PROJECT_ID"
