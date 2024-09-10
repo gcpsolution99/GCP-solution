3559d8a6fbaac8e7ee0c66f2e976fd923e2eb479
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-09-10 21:38:37 +0530

    Update GSP408.sh

--- GSP/GSP408.sh
+++ GSP/GSP408.sh
@@ -1,92 +1,115 @@
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+FROM `data-to-insights.ecommerce.rev_transactions`'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT fullVisitorId hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+fullVisitorId
+, hits_page_pageTitle
+FROM `data-to-insights.ecommerce.rev_transactions` LIMIT 1000'
+
+bq query --use_legacy_sql=false '#standardSQL
 SELECT
 COUNT(DISTINCT fullVisitorId) AS visitor_count
 , hits_page_pageTitle
-FROM \`data-to-insights.ecommerce.rev_transactions\`
-GROUP BY hits_page_pageTitle
-"
+FROM `data-to-insights.ecommerce.rev_transactions`
+GROUP BY hits_page_pageTitle'
 
-
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '#standardSQL
 SELECT
 COUNT(DISTINCT fullVisitorId) AS visitor_count
 , hits_page_pageTitle
-FROM \`data-to-insights.ecommerce.rev_transactions\`
-WHERE hits_page_pageTitle = 'Checkout Confirmation'
-GROUP BY hits_page_pageTitle
-"
+FROM `data-to-insights.ecommerce.rev_transactions`
+WHERE hits_page_pageTitle = "Checkout Confirmation"
+GROUP BY hits_page_pageTitle'
 
-echo "${GREEN}${BOLD}
-
-
-${RESET}"
-
-
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '#standardSQL
 SELECT
 geoNetwork_city,
 SUM(totals_transactions) AS totals_transactions,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors
 FROM
-\`data-to-insights.ecommerce.rev_transactions\`
-GROUP BY geoNetwork_city
-"
+`data-to-insights.ecommerce.rev_transactions`
+GROUP BY geoNetwork_city'
 
-
-
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '#standardSQL
 SELECT
 geoNetwork_city,
 SUM(totals_transactions) AS totals_transactions,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors
 FROM
-\`data-to-insights.ecommerce.rev_transactions\`
+`data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city
-ORDER BY distinct_visitors DESC
-"
+ORDER BY distinct_visitors DESC'
 
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '#standardSQL
 SELECT
 geoNetwork_city,
 SUM(totals_transactions) AS total_products_ordered,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors,
 SUM(totals_transactions) / COUNT( DISTINCT fullVisitorId) AS avg_products_ordered
 FROM
-\`data-to-insights.ecommerce.rev_transactions\`
+`data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city
-ORDER BY avg_products_ordered DESC
-"
+ORDER BY avg_products_ordered DESC'
 
-bq query --use_legacy_sql=false \
-"
-#standardSQL
+bq query --use_legacy_sql=false '#standardSQL
 SELECT
 geoNetwork_city,
 SUM(totals_transactions) AS total_products_ordered,
 COUNT( DISTINCT fullVisitorId) AS distinct_visitors,
 SUM(totals_transactions) / COUNT( DISTINCT fullVisitorId) AS avg_products_ordered
 FROM
-\`data-to-insights.ecommerce.rev_transactions\`
+`data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city
 HAVING avg_products_ordered > 20
-ORDER BY avg_products_ordered DESC
+ORDER BY avg_products_ordered DESC'
+
+bq query --use_legacy_sql=false '#standardSQL
+SELECT
+COUNT(DISTINCT hits_product_v2ProductName) as number_of_products,
+hits_product_v2ProductCategory
+FROM `data-to-insights.ecommerce.rev_transactions`
+WHERE hits_product_v2ProductName IS NOT NULL
+GROUP BY hits_product_v2ProductCategory
+ORDER BY number_of_products DESC
+LIMIT 5'
+
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT hits_product_v2ProductName, hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+GROUP BY 1,2
 "
 
-echo "${GREEN}${BOLD}
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+COUNT(hits_product_v2ProductName) as number_of_products,
+hits_product_v2ProductCategory
+FROM \`data-to-insights.ecommerce.rev_transactions\`
+WHERE hits_product_v2ProductName IS NOT NULL
+GROUP BY hits_product_v2ProductCategory
+ORDER BY number_of_products DESC
+"
 
-Task 4 Completed
-
-${RESET}"
 
 bq query --use_legacy_sql=false \
 "
@@ -100,3 +123,4 @@ GROUP BY hits_product_v2ProductCategory
 ORDER BY number_of_products DESC
 LIMIT 5
 "
+
