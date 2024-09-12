c80899fe99bbdafb48a57db910bddc099b5d21ab
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-09-12 13:01:41 +0530

    Create GSP642.sh

---
+++ GSP/GSP642.sh
@@ -0,0 +1,27 @@
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+gcloud firestore databases create --location=nam5
+
+git clone https://github.com/rosera/pet-theory
+
+cd pet-theory/lab01
+
+npm install @google-cloud/firestore
+
+npm install @google-cloud/logging
+
+curl https://raw.githubusercontent.com/gcpsolution99/GCP-solution/main/importTestData.js > importTestData.js
+
+npm install faker@5.5.3
+
+curl https://raw.githubusercontent.com/gcpsolution99/GCP-solution/main/createTestData.js > createTestData.js
+
+node createTestData 1000
+
+node importTestData customers_1000.csv
+
+npm install csv-parse
+
+node createTestData 20000
+
+node importTestData customers_20000.csv
