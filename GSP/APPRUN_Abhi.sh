0053aaa0feeb1d5fb2471eba4310d5de35f5a1f5
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-09-13 14:03:13 +0530

    Update APPRUN_Abhi.sh

--- GSP/APPRUN_Abhi.sh
+++ GSP/APPRUN_Abhi.sh
@@ -1,50 +1,54 @@
-gcloud services disable pubsub.googleapis.com --force
-
-gcloud services enable pubsub.googleapis.com
-
 gcloud services enable run.googleapis.com
 
-sleep 20
+sleep 10
 
-gcloud config set compute/LOCATION $LOCATION
+gcloud config set compute/region $LOCATION
 
- gcloud run deploy store-service \
-  --image gcr.io/qwiklabs-resources/gsp724-store-service \
-  --LOCATION $LOCATION \
-  --allow-unauthenticated
+gcloud run deploy store-service \
+--image gcr.io/qwiklabs-resources/gsp724-store-service \
+--region $LOCATION \
+--allow-unauthenticated
 
- gcloud run deploy order-service \
-  --image gcr.io/qwiklabs-resources/gsp724-order-service \
-  --LOCATION $LOCATION \
-  --no-allow-unauthenticated
+
+sleep 10
+
+gcloud run deploy order-service \
+--image gcr.io/qwiklabs-resources/gsp724-order-service \
+--region $LOCATION \
+--no-allow-unauthenticated
+
 
 
 gcloud pubsub topics create ORDER_PLACED
 
- gcloud iam service-accounts create pubsub-cloud-run-invoker \
-  --display-name "Order Initiator"
+
+gcloud iam service-accounts create pubsub-cloud-run-invoker \
+--display-name "Order Initiator"
+
 
 gcloud iam service-accounts list --filter="Order Initiator"
 
-sleep 20
 
- gcloud run services add-iam-policy-binding order-service --LOCATION $LOCATION \
-  --member=serviceAccount:pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
-  --role=roles/run.invoker --platform managed
+gcloud run services add-iam-policy-binding order-service --region $LOCATION \
+--member=serviceAccount:pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
+--role=roles/run.invoker --platform managed
 
- PROJECT_NUMBER=$(gcloud projects list \
-  --filter="qwiklabs-gcp" \
-  --format='value(PROJECT_NUMBER)')
+PROJECT_NUMBER=$(gcloud projects list \
+--filter="qwiklabs-gcp" \
+--format='value(PROJECT_NUMBER)')
 
-sleep 20
-
-
- ORDER_SERVICE_URL=$(gcloud run services describe order-service \
-   --LOCATION $LOCATION \
-   --format="value(status.address.url)")
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com \
+--role=roles/iam.serviceAccountTokenCreator
 
 
- gcloud pubsub subscriptions create order-service-sub \
-   --topic ORDER_PLACED \
-   --push-endpoint=$ORDER_SERVICE_URL \
-   --push-auth-service-account=pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
+
+ORDER_SERVICE_URL=$(gcloud run services describe order-service \
+--region $LOCATION \
+--format="value(status.address.url)")
+
+
+gcloud pubsub subscriptions create order-service-sub \
+--topic ORDER_PLACED \
+--push-endpoint=$ORDER_SERVICE_URL \
+--push-auth-service-account=pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
