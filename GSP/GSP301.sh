a8c7a2a107b70704f93db8dd7c0c977d6776fd07
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-09-16 18:57:21 +0530

    Create GSP301.sh

---
+++ GSP/GSP301.sh
@@ -0,0 +1,15 @@
+echo "*** Started ***"
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+gsutil cp gs://sureskills-ql/challenge-labs/ch01-startup-script/install-web.sh gs://$DEVSHELL_PROJECT_ID
+
+gcloud compute instances create abhiarcade --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=n1-standard-1 --tags=http-server --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID/install-web.sh
+
+gcloud compute firewall-rules create allow-http \
+    --allow=tcp:80 \
+    --description="awesome lab" \
+    --direction=INGRESS \
+    --target-tags=http-server
+
+echo "*** Completed !! ***"
