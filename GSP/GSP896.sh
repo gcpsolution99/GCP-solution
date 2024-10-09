3b40b2b9a7c04920e7f932ee6762e502494cd85b
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-10-09 18:06:56 +0530

    Update GSP896.sh

--- GSP/GSP896.sh
+++ GSP/GSP896.sh
@@ -15,8 +15,9 @@ for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
 done
+ZONE="$(gcloud compute instances list --project=$DEVSHELL_PROJECT_ID --format='value(ZONE)' | head -n 1)"
 cat > prepare_disk.sh <<'EOF_END'
-export DOCKER_TAG=gcr.io/ql-shared-resources-test/defect_solution@sha256:776fd8c65304ac017f5b9a986a1b8189695b7abbff6aa0e4ef693c46c7122f4c
+export DOCKER_TAG=gcr.io/ql-shared-resources-test/resistance_solution@sha256:d9095cbd6f7ca69b1a30c58c4272b68062d2004ed259ff0dcb9af0ceb92b393b
 export VISERVING_CPU_DOCKER_WITH_MODEL=${DOCKER_TAG}
 export HTTP_PORT=8602
 export LOCAL_METRIC_PORT=8603
@@ -29,12 +30,13 @@ docker run -v /secrets:/secrets --rm -d --name "test_cpu" \
 --metric_project_id="${PROJECT_ID}" \
 --use_default_credentials=false \
 --service_account_credentials_json=/secrets/assembly-usage-reporter.json
-gsutil cp gs://cloud-training/gsp895/prediction_script.py .
-export PROJECT_ID=$(gcloud config get-value core/project)
-gsutil mb gs://${PROJECT_ID}
-gsutil -m cp gs://cloud-training/gsp897/cosmetic-test-data/*.png \
-gs://${PROJECT_ID}/cosmetic-test-data/
-gsutil cp gs://${PROJECT_ID}/cosmetic-test-data/IMG_07703.png .
+ docker container ls 
+ gsutil cp gs://cloud-training/gsp895/prediction_script.py .
+ export PROJECT_ID=$(gcloud config get-value core/project)
+ gsutil mb gs://${PROJECT_ID}
+ gsutil -m cp gs://cloud-training/gsp895/pcb_images/*.png \
+ gs://${PROJECT_ID}/demo_pcb_images/
+ gsutil cp gs://${PROJECT_ID}/demo_pcb_images/image_275_cx98_cy16_r-5.png .
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo apt install python3.11-venv -y 
@@ -43,14 +45,13 @@ source myvenv/bin/activate
 pip install absl-py  
 pip install numpy 
 pip install requests
-python3 ./prediction_script.py --input_image_file=./IMG_07703.png  --port=8602 --output_result_file=def_prediction_result.json
-python3 ./prediction_script.py --input_image_file=./IMG_07703.png  --port=8602 --num_of_requests=10 --output_result_file=def_latency_result.json
+python3 ./prediction_script.py --input_image_file=./image_275_cx98_cy16_r-5.png  --port=8602 --output_result_file=def_prediction_result.json
+python3 ./prediction_script.py --input_image_file=./image_275_cx98_cy16_r-5.png  --port=8602 --num_of_requests=10 --output_result_file=def_latency_result.json
 export PROJECT_ID=$(gcloud config get-value core/project)
-gsutil cp gs://${PROJECT_ID}/cosmetic-test-data/IMG_0769.png .
-python3 ./prediction_script.py --input_image_file=./IMG_0769.png  --port=8602 --output_result_file=non_def_prediction_result.json
-python3 ./prediction_script.py --input_image_file=./IMG_0769.png  --port=8602 --num_of_requests=10 --output_result_file=non_def_latency_result.json
+gsutil cp gs://${PROJECT_ID}/demo_pcb_images/image_439_cx31_cy-35_r-4.png .
+python3 ./prediction_script.py --input_image_file=./image_439_cx31_cy-35_r-4.png  --port=8602 --output_result_file=non_def_prediction_result.json
+python3 ./prediction_script.py --input_image_file=./image_275_cx98_cy16_r-5.png  --port=8602 --num_of_requests=10 --output_result_file=non_def_latency_result.json
 EOF_END
-export ZONE=$(gcloud compute instances list lab-vm --format 'csv[no-heading](zone)')
 gcloud compute scp prepare_disk.sh lab-vm:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
 gcloud compute ssh lab-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
 pattern=(
