8c5b86ea1460e0dc3e7866c2f37d541e78754979
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-10-10 21:10:25 +0530

    Create CachingContent.sh

---
+++ ARC/CachingContent.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export PROJECT_ID=$(gcloud config get-value project)
+gsutil mb -p $DEVSHELL_PROJECT_ID -c STANDARD -l $LOCATION -b on gs://$DEVSHELL_PROJECT_ID
+gsutil uniformbucketlevelaccess set off gs://$DEVSHELL_PROJECT_ID
+gsutil cp gs://cloud-training/gcpnet/cdn/cdn.png gs://$DEVSHELL_PROJECT_ID
+gsutil acl ch -u AllUsers:R gs://$DEVSHELL_PROJECT_ID/cdn.png
+TOKEN_ID=$(gcloud auth application-default print-access-token)
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN_ID" \
+  -d '{
+    "bucketName": "'"$PROJECT_ID"'",
+    "cdnPolicy": {
+      "cacheMode": "CACHE_ALL_STATIC",
+      "clientTtl": 60,
+      "defaultTtl": 60,
+      "maxTtl": 60,
+      "negativeCaching": false,
+      "serveWhileStale": 0
+    },
+    "compressionMode": "DISABLED",
+    "description": "",
+    "enableCdn": true,
+    "name": "cdn-bucket"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$PROJECT_ID/global/backendBuckets"
+sleep 20
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN_ID" \
+  -d '{
+    "defaultService": "projects/'"$PROJECT_ID"'/global/backendBuckets/cdn-bucket",
+    "name": "cdn-lb"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$PROJECT_ID/global/urlMaps"
+sleep 20
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN_ID" \
+  -d '{
+    "name": "cdn-lb-target-proxy",
+    "urlMap": "projects/'"$PROJECT_ID"'/global/urlMaps/cdn-lb"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$PROJECT_ID/global/targetHttpProxies"
+sleep 20
+curl -X POST -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $TOKEN_ID" \
+  -d '{
+    "IPProtocol": "TCP",
+    "ipVersion": "IPV4",
+    "loadBalancingScheme": "EXTERNAL_MANAGED",
+    "name": "cdn-lb-forwarding-rule",
+    "networkTier": "PREMIUM",
+    "portRange": "80",
+    "target": "projects/'"$PROJECT_ID"'/global/targetHttpProxies/cdn-lb-target-proxy"
+  }' \
+  "https://compute.googleapis.com/compute/v1/projects/$PROJECT_ID/global/forwardingRules"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
