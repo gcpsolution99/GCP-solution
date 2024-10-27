425f7f423fc1440846ca29d513c541333fb72e22
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-10-27 12:47:30 +0530

    Update GSP411.sh

--- GSP/GSP411.sh
+++ GSP/GSP411.sh
@@ -20,7 +20,7 @@ curl -LO raw.githubusercontent.com/gcpsolution99/GCP-solution/refs/heads/main/pr
 bq mk ecommerce
 gsutil mb gs://$DEVSHELL_PROJECT_ID/
 gsutil cp products.csv gs://$DEVSHELL_PROJECT_ID/
-gsutil cp gsp411.csv gs://$DEVSHELL_PROJECT_ID/
+gsutil cp GSP411.csv gs://$DEVSHELL_PROJECT_ID/
 bq --location=US load --source_format=CSV --autodetect --skip_leading_rows=1 ecommerce.products gs://$DEVSHELL_PROJECT_ID/products.csv
 bq --location=US load --source_format=CSV --autodetect --skip_leading_rows=1 ecommerce.products gs://data-insights-course/exports/products.csv
 bq query --use_legacy_sql=false \
