d60b7761618230a0847c2e564f53f6e21267d4ae
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-11-01 17:45:34 +0530

    Update ARC110.sh

--- Create a Streaming Data Lake on Cloud Storage: Challenge Lab/ARC110.sh
+++ Create a Streaming Data Lake on Cloud Storage: Challenge Lab/ARC110.sh
@@ -16,7 +16,7 @@ do
     echo -e "${YELLOW}${line}${NC}"
 done
 gcloud auth list
-gcloud config set compute/region $REGION
+gcloud config set compute/region $region
 gcloud services disable dataflow.googleapis.com
 gcloud services enable \
 dataflow.googleapis.com \
@@ -25,23 +25,23 @@ sleep 45
 PROJECT_ID=$(gcloud config get-value project)
 BUCKET_NAME="${PROJECT_ID}-bucket"
 gsutil mb gs://$BUCKET_NAME
-gcloud pubsub topics create $TOPIC_ID
-if [ "$REGION" == "us-central1" ]; then
+gcloud pubsub topics create $topic
+if [ "$region" == "us-central1" ]; then
   gcloud app create --region us-central
-elif [ "$REGION" == "europe-west1" ]; then
+elif [ "$region" == "europe-west1" ]; then
   gcloud app create --region europe-west
 else
-  gcloud app create --region "$REGION"
+  gcloud app create --region "$region"
 fi
 gcloud scheduler jobs create pubsub publisher-job --schedule="* * * * *" \
-    --topic=$TOPIC_ID --message-body="$MESSAGE"
+    --topic=$topic --message-body="$message"
 #!/bin/bash
 while true; do
-    if gcloud scheduler jobs run publisher-job --location="$REGION"; then
+    if gcloud scheduler jobs run publisher-job --location="$region"; then
         echo "Command executed successfully. Now running next command.."
         break 
     else
-        echo "Retrying.. Please like and subscribe to techcps[https://www.youtube.com/@techcps]."
+        echo ""
         sleep 10 
     fi
 done
@@ -52,8 +52,8 @@ cd python-docs-samples/pubsub/streaming-analytics
 pip install -U -r requirements.txt
 python PubSubToGCS.py \
 --project=$PROJECT_ID \
---region=$REGION \
---input_topic=projects/$PROJECT_ID/topics/$TOPIC_ID \
+--region=$region \
+--input_topic=projects/$PROJECT_ID/topics/$topic \
 --output_path=gs://$BUCKET_NAME/samples/output \
 --runner=DataflowRunner \
 --window_size=2 \
@@ -61,7 +61,7 @@ python PubSubToGCS.py \
 --temp_location=gs://$BUCKET_NAME/temp
 EOF_CP
 chmod +x abhi.sh
-docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e REGION=$REGION -e TOPIC_ID=$TOPIC_ID -v $(pwd)/abhi.sh:/abhi.sh python:3.7 /bin/bash -c "/abhi.sh"
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e region=$region -e topic=$topic -v $(pwd)/abhi.sh:/abhi.sh python:3.7 /bin/bash -c "/abhi.sh"
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
