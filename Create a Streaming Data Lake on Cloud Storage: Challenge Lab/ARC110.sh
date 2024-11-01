086e2bc26ddd36695e43faf3f230937251a7791f
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-11-01 17:41:47 +0530

    Update ARC110.sh

--- Create a Streaming Data Lake on Cloud Storage: Challenge Lab/ARC110.sh
+++ Create a Streaming Data Lake on Cloud Storage: Challenge Lab/ARC110.sh
@@ -1,26 +1,31 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
 gcloud auth list
-
 gcloud config set compute/region $REGION
-
 gcloud services disable dataflow.googleapis.com
 gcloud services enable \
 dataflow.googleapis.com \
 cloudscheduler.googleapis.com
-
-
 sleep 45
-
-
 PROJECT_ID=$(gcloud config get-value project)
 BUCKET_NAME="${PROJECT_ID}-bucket"
-
-
 gsutil mb gs://$BUCKET_NAME
-
 gcloud pubsub topics create $TOPIC_ID
-
-
-# Set the App Engine region variable
 if [ "$REGION" == "us-central1" ]; then
   gcloud app create --region us-central
 elif [ "$REGION" == "europe-west1" ]; then
@@ -28,31 +33,20 @@ elif [ "$REGION" == "europe-west1" ]; then
 else
   gcloud app create --region "$REGION"
 fi
- 
-
 gcloud scheduler jobs create pubsub publisher-job --schedule="* * * * *" \
     --topic=$TOPIC_ID --message-body="$MESSAGE"
-
-
-
 #!/bin/bash
-
 while true; do
     if gcloud scheduler jobs run publisher-job --location="$REGION"; then
         echo "Command executed successfully. Now running next command.."
         break 
     else
-        echo "Retrying.. "
+        echo "Retrying.. Please like and subscribe to techcps[https://www.youtube.com/@techcps]."
         sleep 10 
     fi
 done
-
-
-
-
 cat > abhi.sh <<EOF_CP
 #!/bin/bash
-
 git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
 cd python-docs-samples/pubsub/streaming-analytics
 pip install -U -r requirements.txt
@@ -66,8 +60,17 @@ python PubSubToGCS.py \
 --num_shards=2 \
 --temp_location=gs://$BUCKET_NAME/temp
 EOF_CP
-
 chmod +x abhi.sh
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e REGION=$REGION -e TOPIC_ID=$TOPIC_ID -v $(pwd)/abhi.sh:/abhi.sh python:3.7 /bin/bash -c "/abhi.sh"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
 
-
-docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e REGION=$REGION -e TOPIC_ID=$TOPIC_ID -v $(pwd)/techcps.sh:/techcps.sh python:3.7 /bin/bash -c "/techcps.sh"
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
