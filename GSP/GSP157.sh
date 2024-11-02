5da56237d93302ea0461740d1d59b2e4d5eac06d
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-11-02 13:15:32 +0530

    Create GSP157.sh

---
+++ GSP/GSP157.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud compute instances create www-1 \
+--image-family debian-11 \
+--image-project debian-cloud \
+--zone $ZONE_1 \
+--tags http-tag \
+--metadata startup-script="#! /bin/bash
+apt-get update
+apt-get install apache2 -y
+service apache2 restart
+Code
+EOF"
+
+sleep 15
+
+gcloud compute instances create www-2 \
+--image-family debian-11 \
+--image-project debian-cloud \
+--zone $ZONE_1 \
+--tags http-tag \
+--metadata startup-script="#! /bin/bash
+apt-get update
+apt-get install apache2 -y
+service apache2 restart
+Code
+EOF"
+
+
+sleep 15
+
+gcloud compute instances create www-3 \
+--image-family debian-11 \
+--image-project debian-cloud \
+--zone $ZONE_2 \
+--tags http-tag \
+--metadata startup-script="#! /bin/bash
+apt-get update
+apt-get install apache2 -y
+service apache2 restart
+Code
+EOF"
+
+sleep 15
+
+gcloud compute instances create www-4 \
+--image-family debian-11 \
+--image-project debian-cloud \
+--zone $ZONE_2 \
+--tags http-tag \
+--metadata startup-script="#! /bin/bash
+apt-get update
+apt-get install apache2 -y
+service apache2 restart
+Code
+EOF"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
