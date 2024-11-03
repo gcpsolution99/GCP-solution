cfe86d9d6453a9c86e36c6599b14617e9f99af2c
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-11-03 12:05:34 +0530

    Update GSP207.sh

--- GSP/GSP207.sh
+++ GSP/GSP207.sh
@@ -1,13 +1,22 @@
-gcloud config set compute/region $REGION
+#!/bin/bash
 
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+gcloud config set compute/region $LOCATION
 gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket/
-
 gcloud services disable dataflow.googleapis.com
-
+sleep 20
 gcloud services enable dataflow.googleapis.com
-
-docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e REGION=$REGION python:3.9 /bin/bash -c '
-
+sleep 20
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e LOCATION=$LOCATION python:3.9 /bin/bash -c '
 pip install "apache-beam[gcp]"==2.42.0 && \
 python -m apache_beam.examples.wordcount --output OUTPUT_FILE && \
 HUSTLER=gs://$DEVSHELL_PROJECT_ID-bucket && \
@@ -16,5 +25,20 @@ python -m apache_beam.examples.wordcount --project $DEVSHELL_PROJECT_ID \
   --staging_location $HUSTLER/staging \
   --temp_location $HUSTLER/temp \
   --output $HUSTLER/results/output \
-  --region $REGION
+  --region $LOCATION
 '
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
