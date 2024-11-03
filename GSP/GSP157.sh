262d49906d9feffe3c9232af22480c37662527ba
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-11-03 10:42:00 +0530

    Update GSP157.sh

--- GSP/GSP157.sh
+++ GSP/GSP157.sh
@@ -40,9 +40,7 @@ service apache2 restart
 Code
 EOF"
 
-
 sleep 15
-
 gcloud compute instances create www-3 \
 --image-family debian-11 \
 --image-project debian-cloud \
@@ -54,9 +52,7 @@ apt-get install apache2 -y
 service apache2 restart
 Code
 EOF"
-
 sleep 15
-
 gcloud compute instances create www-4 \
 --image-family debian-11 \
 --image-project debian-cloud \
@@ -69,6 +65,54 @@ service apache2 restart
 Code
 EOF"
 gcloud compute instances list
+gcloud compute firewall-rules create www-firewall \
+--target-tags http-tag --allow tcp:80
+gcloud compute addresses create lb-ip-cr \
+--ip-version=IPV4 \
+--global
+gcloud compute instance-groups unmanaged create $REGION_1-resources-w --zone $ZONE_1
+gcloud compute instance-groups unmanaged create $REGION_2-resources-w --zone $ZONE_2
+gcloud compute instance-groups unmanaged add-instances $REGION_1-resources-w \
+--instances www-1,www-2 \
+--zone $ZONE_1
+gcloud compute instance-groups unmanaged add-instances $REGION_2-resources-w \
+--instances www-3,www-4 \
+--zone $ZONE_2
+gcloud compute health-checks create http http-basic-check
+gcloud compute instance-groups unmanaged set-named-ports $REGION_1-resources-w \
+--named-ports http:80 \
+--zone $ZONE_1
+gcloud compute instance-groups unmanaged set-named-ports $REGION_2-resources-w \
+--named-ports http:80 \
+--zone $ZONE_2
+gcloud compute backend-services create web-map-backend-service \
+--protocol HTTP \
+--health-checks http-basic-check \
+--global
+gcloud compute backend-services add-backend web-map-backend-service \
+--balancing-mode UTILIZATION \
+--max-utilization 0.8 \
+--capacity-scaler 1 \
+--instance-group $REGION_1-resources-w \
+--instance-group-zone $ZONE_1 \
+--global
+gcloud compute backend-services add-backend web-map-backend-service \
+--balancing-mode UTILIZATION \
+--max-utilization 0.8 \
+--capacity-scaler 1 \
+--instance-group $REGION_2-resources-w \
+--instance-group-zone $ZONE_2 \
+--global
+gcloud compute url-maps create web-map \
+--default-service web-map-backend-service
+gcloud compute target-http-proxies create http-lb-proxy \
+--url-map web-map
+EXTERNAL_IP=$(gcloud compute addresses list --format="get(ADDRESS)")
+gcloud compute forwarding-rules create http-cr-rule \
+--address $EXTERNAL_IP \
+--global \
+--target-http-proxy http-lb-proxy \
+--ports 80
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
