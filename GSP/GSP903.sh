c47e0adb67b67427d52436025c46a254f415ce7c
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-11-16 11:55:35 +0530

    Update GSP903.sh

--- GSP/GSP903.sh
+++ GSP/GSP903.sh
@@ -12,23 +12,29 @@ for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
 done
-gcloud config set compute/region $LOCATION
+gcloud config set compute/region $REGION
 gcloud services disable dataflow.googleapis.com
 gcloud services enable dataflow.googleapis.com
 gcloud services enable cloudscheduler.googleapis.com
-sleep 20
+sleep 60
 PROJECT_ID=$(gcloud config get-value project)
 BUCKET_NAME="${PROJECT_ID}-bucket"
 TOPIC_ID=my-id
 gsutil mb gs://$BUCKET_NAME
 gcloud pubsub topics create $TOPIC_ID
-gcloud app create --region "$LOCATION"
+if [ "$REGION" == "us-central1" ]; then
+  gcloud app create --region us-central
+elif [ "$REGION" == "europe-west1" ]; then
+  gcloud app create --region europe-west
+else
+  gcloud app create --region "$REGION"
+fi
 gcloud scheduler jobs create pubsub publisher-job --schedule="* * * * *" \
     --topic=$TOPIC_ID --message-body="Hello!"
 sleep 90
-gcloud scheduler jobs run publisher-job --location=$LOCATION
+gcloud scheduler jobs run publisher-job --location=$REGION
 sleep 90
-gcloud scheduler jobs run publisher-job --location=$LOCATION
+gcloud scheduler jobs run publisher-job --location=$REGION
 cat > automate_commands.sh <<EOF_END
 #!/bin/bash
 git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
@@ -36,7 +42,7 @@ cd python-docs-samples/pubsub/streaming-analytics
 pip install -U -r requirements.txt
 python PubSubToGCS.py \
 --project=$PROJECT_ID \
---region=$LOCATION \
+--region=$REGION \
 --input_topic=projects/$PROJECT_ID/topics/$TOPIC_ID \
 --output_path=gs://$BUCKET_NAME/samples/output \
 --runner=DataflowRunner \
@@ -45,6 +51,7 @@ python PubSubToGCS.py \
 --temp_location=gs://$BUCKET_NAME/temp
 EOF_END
 chmod +x automate_commands.sh
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e REGION=$REGION -e TOPIC_ID=$TOPIC_ID -v $(pwd)/automate_commands.sh:/automate_commands.sh python:3.7 /bin/bash -c "/automate_commands.sh"
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
