6c43df433cebe5be6cb454f6a2e2a9b9f7bdea56
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-12-05 19:18:34 +0530

    Create cs4.sh

---
+++ GSP/cs4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export PROJECT=$(gcloud projects list --format="value(PROJECT_ID)")
+gsutil setmeta -h "Content-Type:text/html" gs://$PROJECT-bucket/index.html
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
