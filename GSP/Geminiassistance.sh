b707b24e3d5037212285b1a0b61f103ecee413f1
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-12-06 21:13:53 +0530

    Update Geminiassistance.sh

--- GSP/Geminiassistance.sh
+++ GSP/Geminiassistance.sh
@@ -15,15 +15,14 @@ for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
 done
-PROJECT_ID=$(gcloud config get-value project)
-echo "PROJECT_ID=${PROJECT_ID}"
-echo "REGION=${REGION}"
-USER=$(gcloud config get-value account 2> /dev/null)
-echo "USER=${USER}"
-gcloud services enable cloudaicompanion.googleapis.com --project ${PROJECT_ID}
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export PROJECT_ID=$(gcloud config get-value project)
+export USER=$(gcloud config get-value account 2> /dev/null)
+gcloud services enable cloudaicompanion.googleapis.com
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/cloudaicompanion.user
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/serviceusage.serviceUsageViewer
-bq mk --dataset bqml_tutorial
+bq mk bqml_tutorial
 bq query --use_legacy_sql=false \
 "
 SELECT u.id as user_id, u.first_name, u.last_name, avg(oi.sale_price) as avg_sale_price   
@@ -48,7 +47,44 @@ JOIN
 GROUP BY
   created_at_day,
   product_id
-"  
+"
+bq query --use_legacy_sql=false '
+SELECT 
+    u.id AS user_id, 
+    u.first_name, 
+    u.last_name, 
+    AVG(oi.sale_price) AS avg_sale_price
+FROM 
+    `bigquery-public-data.thelook_ecommerce.users` AS u
+JOIN 
+    `bigquery-public-data.thelook_ecommerce.order_items` AS oi
+ON 
+    u.id = oi.user_id
+GROUP BY 
+    u.id, u.first_name, u.last_name
+ORDER BY 
+    avg_sale_price DESC
+LIMIT 10
+'
+bq query --use_legacy_sql=false '
+SELECT
+    DATE(order_items.created_at) AS order_date,
+    order_items.product_id,
+    products.name AS product_name,
+    ROUND(SUM(order_items.sale_price), 2) AS total_sales
+FROM
+    `bigquery-public-data.thelook_ecommerce.order_items` AS order_items
+LEFT JOIN
+    `bigquery-public-data.thelook_ecommerce.products` AS products
+ON
+    order_items.product_id = products.id
+GROUP BY
+    order_date,
+    order_items.product_id,
+    product_name
+ORDER BY
+    total_sales DESC
+'
 bq query --use_legacy_sql=false \
 "
 CREATE MODEL bqml_tutorial.sales_forecasting_model
@@ -66,12 +102,11 @@ AS t2
 ON t1.product_id = t2.id
 GROUP BY 2, 3;
 "
-export PROJECT_ID=$(gcloud config get-value project)
 bq query --use_legacy_sql=false "
 SELECT
   *
 FROM
-  ML.FORECAST(MODEL \`$PROJECT_ID.bqml_tutorial.sales_forecasting_model\`,
+  ML.FORECAST(MODEL \`$DEVSHELL_PROJECT_ID.bqml_tutorial.sales_forecasting_model\`,
     STRUCT(
       7 AS horizon,
       0.95 AS confidence_level
