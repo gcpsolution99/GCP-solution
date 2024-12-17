e398bfe2d0571e103d206e4c2a22cd044cb862d0
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2024-12-17 20:03:37 +0530

    Update GSP367.sh

--- GSP/GSP367.sh
+++ GSP/GSP367.sh
@@ -27,21 +27,14 @@ gcloud services enable cloudbuild.googleapis.com
 gcloud services enable geocoding-backend.googleapis.com 
 gcloud services enable eventarc.googleapis.com
 gcloud services enable run.googleapis.com
-
 gcloud auth list
-
 export PROJECT_ID=$(gcloud config get-value core/project)
-
 gcloud services enable documentai.googleapis.com --project $DEVSHELL_PROJECT_ID
-
-sleep 10
-
+sleep 20
   mkdir ./document-ai-challenge
   gsutil -m cp -r gs://spls/gsp367/* \
     ~/document-ai-challenge/
-
 ACCESS_CP=$(gcloud auth application-default print-access-token)
-
 curl -X POST \
   -H "Authorization: Bearer $ACCESS_CP" \
   -H "Content-Type: application/json" \
@@ -50,49 +43,27 @@ curl -X POST \
     "type": "FORM_PARSER_PROCESSOR"
   }' \
   "https://documentai.googleapis.com/v1/projects/$PROJECT_ID/locations/us/processors"
-
-
 gsutil mb -c standard -l $REGION -b on gs://$PROJECT_ID-input-invoices
-
 gsutil mb -c standard -l $REGION -b on gs://$PROJECT_ID-output-invoices
-
 gsutil mb -c standard -l $REGION -b on gs://$PROJECT_ID-archived-invoices
-
-
-
-
 bq --location=US mk  -d \
  --description "Form Parser Results" \
  ${PROJECT_ID}:invoice_parser_results
- 
 cd ~/document-ai-challenge/scripts/table-schema
- 
 bq mk --table \
 invoice_parser_results.doc_ai_extracted_entities \
 doc_ai_extracted_entities.json
-
-
-
 cd ~/document-ai-challenge/scripts
-
 PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
-
 SERVICE_ACCOUNT=$(gcloud storage service-agent --project=$PROJECT_ID)
-
 gcloud projects add-iam-policy-binding $PROJECT_ID \
   --member serviceAccount:$SERVICE_ACCOUNT \
   --role roles/pubsub.publisher
-
-
 export CLOUD_FUNCTION_LOCATION=$REGION
-
 echo $CLOUD_FUNCTION_LOCATION
-
 sleep 30
-
 #!/bin/bash
-
 deploy_function() {
   gcloud functions deploy process-invoices \
   --gen2 \
@@ -118,10 +89,6 @@ while [ "$deploy_success" = false ]; do
     sleep 10
   fi
 done
-
-
-
-
 PROCESSOR_ID=$(curl -X GET \
   -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
   -H "Content-Type: application/json" \
@@ -151,8 +118,6 @@ gcloud functions deploy process-invoices \
   --trigger-event=google.storage.object.finalize \
   --update-env-vars=PROCESSOR_ID=${PROCESSOR_ID},PARSER_LOCATION=us,PROJECT_ID=${PROJECT_ID} \
   --service-account=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
-
-
 export PROJECT_ID=$(gcloud config get-value core/project)
 gsutil -m cp -r gs://cloud-training/gsp367/* \
 ~/document-ai-challenge/invoices gs://${PROJECT_ID}-input-invoices/
@@ -163,7 +128,6 @@ pattern=(
 "**                                                      **"
 "**********************************************************"
 )
-
 for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
