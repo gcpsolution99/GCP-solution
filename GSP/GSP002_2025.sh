ca2bfcb16b8c820f967e5c8b96b782e61c9d86a3
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-01-20 19:37:15 +0530

    Create GSP002_2025.sh

---
+++ GSP/GSP002_2025.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud compute instances create gcelab2 --machine-type e2-medium --zone $ZONE
+
+gcloud compute instances add-tags gcelab2 --zone $ZONE --tags http-server,https-server
+
+gcloud compute firewall-rules create default-allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
