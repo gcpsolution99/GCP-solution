01b0ef0b4052d3381a26f456772e86185f794841
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-02-03 21:41:21 +0530

    Create arc1200.sh

---
+++ GSP/arc1200.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud storage buckets create gs://$(gcloud config get-value project)-bucket --location=$ZONE
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
