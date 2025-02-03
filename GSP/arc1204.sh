e64ff40a51a374ceeb71f9764033df249aaf8db5
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-02-03 23:07:31 +0530

    Create arc1204.sh

---
+++ GSP/arc1204.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud pubsub topics create sports_topic
+sleep 10
+gcloud pubsub topics create app_topic
+sleep 10
+gcloud pubsub subscriptions create app_subscription --topic=app_topic
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
