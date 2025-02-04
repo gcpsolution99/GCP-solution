6822be81dcb88f0ac04d69507733082f262fd2e3
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-02-04 19:24:47 +0530

    Create arc1203.sh

---
+++ GSP/arc1203.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud projects add-iam-policy-binding $(gcloud config get-value project) \
+    --member="user:$ID1" \
+    --role="roles/editor"
+
+sleep 20
+
+gcloud projects add-iam-policy-binding $(gcloud config get-value project) \
+    --member="user:$ID2" \
+    --role="roles/compute.admin"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
