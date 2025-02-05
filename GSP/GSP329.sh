fc3ceba0ea34d612805ec6459628dac070b6e1c2
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-02-05 20:50:47 +0530

    Create GSP329.sh

---
+++ GSP/GSP329.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export LANGUAGE LOCAL BIGQUERY_ROLE CLOUD_STORAGE_ROLE
+gcloud auth list
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+gcloud iam service-accounts create sample-sa
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:sample-sa@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=$BIGQUERY_ROLE
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:sample-sa@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=$CLOUD_STORAGE_ROLE
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:sample-sa@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=roles/serviceusage.serviceUsageConsumer
+sleep 130
+gcloud iam service-accounts keys create sample-sa-key.json --iam-account sample-sa@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
+export GOOGLE_APPLICATION_CREDENTIALS=${PWD}/sample-sa-key.json
+wget https://raw.githubusercontent.com/gcpsolution99/GCP-solution/refs/heads/main/analyze-images-v21.py
+sed -i "s/'en'/'${LOCAL}'/g" analyze-images-v21.py
+python3 analyze-images-v21.py
+python3 analyze-images-v21.py $DEVSHELL_PROJECT_ID $DEVSHELL_PROJECT_ID
+bq query --use_legacy_sql=false "SELECT locale,COUNT(locale) as lcount FROM image_classification_dataset.image_text_detail GROUP BY locale ORDER BY lcount DESC"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
