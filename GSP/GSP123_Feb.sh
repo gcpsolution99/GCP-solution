e0b7fd1e4409f3b9894ff7eab030313b5df12d55
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-02-07 19:39:55 +0530

    Update GSP123_Feb.sh

--- GSP/GSP123_Feb.sh
+++ GSP/GSP123_Feb.sh
@@ -12,35 +12,28 @@ for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
 done
-export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+PROJECT_ID=`gcloud config get-value project`
 PROJECT_NUMBER=$(gcloud projects describe $(gcloud config get-value project) --format="value(projectNumber)")
-
-gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+echo $PROJECT_NUMBER
+gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
     --role="roles/storage.admin"
-    
-gcloud dataproc clusters create qlab \
-  --enable-component-gateway \
-  --region $REGION \
-  --zone $ZONE \
-  --master-machine-type e2-standard-4 \
-  --master-boot-disk-type pd-balanced \
-  --master-boot-disk-size 100 \
-  --num-workers 2 \
-  --worker-machine-type e2-standard-2 \
-  --worker-boot-disk-size 100 \
-  --image-version 2.2-debian12 \
-  --project $DEVSHELL_PROJECT_ID
-  
-sleep 30
-
+sleep 120
+gcloud dataproc clusters create qlab --enable-component-gateway --region $REGION --zone $ZONE --master-machine-type e2-standard-4 --master-boot-disk-type pd-balanced --master-boot-disk-size 100 --num-workers 2 --worker-machine-type e2-standard-2 --worker-boot-disk-size 100 --image-version 2.2-debian12 --project $PROJECT_I
+sleep 130
+gcloud dataproc clusters delete qlab --region $REGION --quiet
+gcloud dataproc clusters create qlab --enable-component-gateway --region $REGION --zone $ZONE --master-machine-type e2-standard-4 --master-boot-disk-type pd-balanced --master-boot-disk-size 100 --num-workers 2 --worker-machine-type e2-standard-2 --worker-boot-disk-size 100 --image-version 2.2-debian12 --project $PROJECT_ID
+sleep 130
 gcloud dataproc jobs submit spark \
-  --region $REGION \
-  --cluster qlab \
-  --class org.apache.spark.examples.SparkPi \
-  --jars file:///usr/lib/spark/examples/jars/spark-examples.jar \
-  -- 1000
+    --cluster=qlab \
+    --region=$REGION \
+    --class=org.apache.spark.examples.SparkPi \
+    --jars=file:///usr/lib/spark/examples/jars/spark-examples.jar \
+    -- 1000
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
