4d702437d8db5245e16e2e4059f3e63eb221e134
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-03-06 19:24:22 +0530

    Create GSP1125.sh

---
+++ GSP/GSP1125.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+gcloud projects get-iam-policy $(gcloud config get-value project) \
+    --format=json > policy.json
+    
+jq '{ 
+  "auditConfigs": [ 
+    { 
+      "service": "cloudresourcemanager.googleapis.com", 
+      "auditLogConfigs": [ 
+        { 
+          "logType": "ADMIN_READ" 
+        } 
+      ] 
+    } 
+  ] 
+} + .' policy.json > updated_policy.json
+
+gcloud projects set-iam-policy $(gcloud config get-value project) updated_policy.json
+gcloud services enable securitycenter.googleapis.com --project=$DEVSHELL_PROJECT_ID
+sleep 20
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member=user:demouser1@gmail.com --role=roles/bigquery.admin
+
+gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member=user:demouser1@gmail.com --role=roles/bigquery.admin
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USER_EMAIL \
+  --role=roles/cloudresourcemanager.projectIamAdmin 2>/dev/null
+gcloud compute instances create instance-1 \
+--zone=$ZONE \
+--machine-type=e2-medium \
+--network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+--metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD \
+--scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230912,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced
+
+gcloud dns --project=$DEVSHELL_PROJECT_ID policies create dns-test-policy --description="quickgcplab" --networks="default" --private-alternative-name-servers="" --no-enable-inbound-forwarding --enable-logging
+
+sleep 30
+gcloud compute ssh instance-1 --zone=$ZONE --tunnel-through-iap --project "$DEVSHELL_PROJECT_ID" --quiet --command "gcloud projects get-iam-policy \$(gcloud config get project) && curl etd-malware-trigger.goog"
+
+function check_progress {
+    while true; do
+        echo
+        echo -n "Check my progress for Task 1 & Task 2? (Y/N)"
+        read -r user_input
+        if [[ "$user_input" == "Y" || "$user_input" == "y" ]]; then
+            break
+        elif [[ "$user_input" == "N" || "$user_input" == "n" ]]; then
+           
+        else
+    
+        fi
+    done
+}
+gcloud compute instances delete instance-1 --zone=$ZONE --quiet
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
