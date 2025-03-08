f513d95a337679a5ec740c4f5605d942cb96ba18
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-03-08 18:14:09 +0530

    Update GSP1125.sh

--- GSP/GSP1125.sh
+++ GSP/GSP1125.sh
@@ -14,11 +14,13 @@ do
 done
 export ZONE=$(gcloud compute project-info describe \
 --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
 export REGION=$(gcloud compute project-info describe \
 --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
 gcloud projects get-iam-policy $(gcloud config get-value project) \
     --format=json > policy.json
-    
+
 jq '{ 
   "auditConfigs": [ 
     { 
@@ -33,16 +35,21 @@ jq '{
 } + .' policy.json > updated_policy.json
 
 gcloud projects set-iam-policy $(gcloud config get-value project) updated_policy.json
+
 gcloud services enable securitycenter.googleapis.com --project=$DEVSHELL_PROJECT_ID
+
 sleep 20
+
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
 --member=user:demouser1@gmail.com --role=roles/bigquery.admin
 
 gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
 --member=user:demouser1@gmail.com --role=roles/bigquery.admin
+
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
   --member=user:$USER_EMAIL \
   --role=roles/cloudresourcemanager.projectIamAdmin 2>/dev/null
+
 gcloud compute instances create instance-1 \
 --zone=$ZONE \
 --machine-type=e2-medium \
@@ -50,26 +57,26 @@ gcloud compute instances create instance-1 \
 --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD \
 --scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230912,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced
 
-gcloud dns --project=$DEVSHELL_PROJECT_ID policies create dns-test-policy --description="gcplab" --networks="default" --private-alternative-name-servers="" --no-enable-inbound-forwarding --enable-logging
+gcloud dns --project=$DEVSHELL_PROJECT_ID policies create dns-test-policy --description="quickgcplab" --networks="default" --private-alternative-name-servers="" --no-enable-inbound-forwarding --enable-logging
 
 sleep 30
+
 gcloud compute ssh instance-1 --zone=$ZONE --tunnel-through-iap --project "$DEVSHELL_PROJECT_ID" --quiet --command "gcloud projects get-iam-policy \$(gcloud config get project) && curl etd-malware-trigger.goog"
 
 function check_progress {
     while true; do
         echo
-        echo -n "Check my progress for Task 1 & Task 2? (Y/N)"
+        echo -n "Checked progress for Task 1 & 2 ? (Y/N)"
         read -r user_input
         if [[ "$user_input" == "Y" || "$user_input" == "y" ]]; then
             break
-        elif [[ "$user_input" == "N" || "$user_input" == "n" ]]; then
-           
-        else
-    
-        fi
     done
 }
+
+check_progress
 gcloud compute instances delete instance-1 --zone=$ZONE --quiet
+
+echo
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
