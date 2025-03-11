01c442ffa1630009bfd6a27fef14afd6e9d1339a
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-03-11 21:26:33 +0530

    Create GSP373.sh

---
+++ GSP/GSP373.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+gcloud services enable iap.googleapis.com
+
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+
+cd python-docs-samples/appengine/standard_python3/hello_world/
+
+gcloud app create --project=$(gcloud config get-value project) --region=$REGION
+
+gcloud app deploy --quiet
+
+export AUTH_DOMAIN=$(gcloud config get-value project).uc.r.appspot.com
+
+EMAIL="$(gcloud config get-value core/account)"
+
+cat > details.json << EOF
+  App name: AbhiArcadeSolution
+  Authorized domains: $AUTH_DOMAIN
+  Developer contact email: $EMAIL
+EOF
+
+cat details.json
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
