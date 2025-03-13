fad89c57ade4769774add387f71df94a6168266f
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-03-13 19:29:36 +0530

    Create GSP866.sh

---
+++ GSP/GSP866.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+SELECT
+  *
+FROM
+  `bigquery-public-data.new_york_citibike.citibike_stations`
+LIMIT
+  10
+
+
+  -- Finds Citi Bike stations with > 30 bikes
+SELECT
+  ST_GeogPoint(longitude, latitude)  AS WKT,
+  num_bikes_available
+FROM
+  `bigquery-public-data.new_york.citibike_stations`
+WHERE num_bikes_available > 30
+
+
+-- Finds Citi Bike stations with > 30 bikes
+SELECT
+  ST_GeogPoint(longitude, latitude)  AS WKT,
+  num_bikes_available
+FROM
+  `bigquery-public-data.new_york.citibike_stations`
+WHERE num_bikes_available > 30
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
