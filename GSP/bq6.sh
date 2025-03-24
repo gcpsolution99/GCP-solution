edd02f75eb15a572fe740bf6884255db5c1d53a8
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-03-24 20:54:46 +0530

    Update bq6.sh

--- GSP/bq6.sh
+++ GSP/bq6.sh
@@ -12,13 +12,17 @@ for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
 done
-bq mk ecommerce
+bq query --use_legacy_sql=false '
+CREATE OR REPLACE TABLE `qwiklabs-gcp-02-53be2cb9be8a.ecommerce.backup_orders` AS
+SELECT *
+FROM `qwiklabs-gcp-02-53be2cb9be8a.ecommerce.customer_orders`;
+'
 
-bq mk --table ecommerce.customer_orders \
-    customer_orders_id:INTEGER,device_id:STRING,username:STRING,department:STRING,office:STRING
-
-bq mk --table ecommerce.backup_orders \
-    backup_orders_id:INTEGER,device_id:STRING,username:STRING,department:STRING,office:STRING
+bq query --use_legacy_sql=false --schedule "every 1 month" --display_name "Monthly Backup of Customer Data" 
+--description "This query backs up customer_orders to backup_orders every month" \
+'CREATE OR REPLACE TABLE `qwiklabs-gcp-02-ad19f450d7b8.ecommerce.backup_orders` AS
+SELECT *
+FROM `qwiklabs-gcp-02-ad19f450d7b8.ecommerce.customer_orders`;'
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
