1bdf12a40b5fda45e08fd8163ce3022e01d733c4
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-03-25 18:36:03 +0530

    Update bq5.sh

--- GSP/bq5.sh
+++ GSP/bq5.sh
@@ -12,28 +12,10 @@ for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
 done
-bq load --source_format=CSV \
-  --skip_leading_rows=1 \
-  --autodetect \
-  '$project_id:customer_details.customers' \
-  gs://$project_id-bucket/customers.csv
-
-
-
-bq query --use_legacy_sql=false \
-  'CREATE OR REPLACE TABLE `$project_id.customer_details.male_customers` AS
-   SELECT customer_id, gender, other_columns
-   FROM `$project_id.customer_details.customers`
-   WHERE gender = "Male";'
-
-
-
-bq extract \
-  --destination_format=CSV \
-  --field_delimiter=',' \
-  --print_header=true \
-  '$project_id:customer_details.male_customers' \
-  gs://$project_id-bucket/exported_male_customers.csv
+PROJECT_ID=$(gcloud config get-value project)
+bq load --autodetect --source_format=CSV customer_details.customers customers.csv
+bq query --use_legacy_sql=false 'CREATE OR REPLACE TABLE customer_details.male_customers AS SELECT CustomerID, Gender FROM customer_details.customers WHERE Gender = "Male"'
+bq extract --destination_format=CSV customer_details.male_customers gs://${PROJECT_ID}-bucket/exported_male_customers.csv 
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
