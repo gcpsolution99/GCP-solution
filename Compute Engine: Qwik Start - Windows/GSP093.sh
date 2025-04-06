81c0f1c454916cd96fbaf5ce98a849eeb179e95d
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-06 12:43:02 +0530

    Update GSP093.sh

--- Compute Engine: Qwik Start - Windows/GSP093.sh
+++ Compute Engine: Qwik Start - Windows/GSP093.sh
@@ -14,13 +14,13 @@ do
 done
 gcloud auth list
 
-gcloud compute instances create arcadecrew --project=$DEVSHELL_PROJECT_ID --zone $ZONE --machine-type=e2-medium --create-disk=auto-delete=yes,boot=yes,device-name=arcadecrew,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20230913,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced 
+gcloud compute instances create abhi --project=$DEVSHELL_PROJECT_ID --zone $ZONE --machine-type=e2-medium --create-disk=auto-delete=yes,boot=yes,device-name=abhi,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20230913,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced 
 
 sleep 30
 
-gcloud compute instances get-serial-port-output arcadecrew --zone=$ZONE
+gcloud compute instances get-serial-port-output abhi --zone=$ZONE
 
-gcloud compute reset-windows-password arcadecrew --zone $ZONE --user admin --quiet
+gcloud compute reset-windows-password abhi --zone $ZONE --user admin --quiet
 
 gcloud compute ssh my-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
 pattern=(
