dc60056e2d1e94d63eb93960f7a48fe73658d261
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-07 21:06:48 +0530

    Update GSP1048.sh

--- GSP/GSP1048.sh
+++ GSP/GSP1048.sh
@@ -1,13 +1,27 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
 gcloud spanner instances create banking-instance \
 --config=regional-$REGION  \
---description="quicklab_1" \
+--description="abhi" \
 --nodes=1
 
 gcloud spanner databases create banking-db --instance=banking-instance
 
 gcloud spanner instances create banking-instance-2 \
 --config=regional-$REGION  \
---description="quicklab_2" \
+--description="abhi1" \
 --nodes=2
 
 gcloud spanner databases create banking-db-2 --instance=banking-instance-2
@@ -18,7 +32,14 @@ gcloud spanner databases ddl update banking-db --instance=banking-instance --ddl
   Location STRING(MAX) NOT NULL,
 ) PRIMARY KEY (CustomerId);"
 
-
-Task Completed
-
-Lab Completed !!!
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
