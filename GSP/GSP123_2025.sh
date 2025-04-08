7d64a0e973d2eb1d6220aa5b6751c0a510d611c1
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-08 09:09:51 +0530

    Create GSP123_2025.sh

---
+++ GSP/GSP123_2025.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export CLUSTER_NAME 
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+
+PROJECT_NUMBER=$(gcloud projects describe $(gcloud config get-value project) --format="value(projectNumber)")
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
+    --role="roles/storage.admin"
+
+sleep 60
+
+#!/bin/bash
+
+echo "$CLUSTER_NAME"
+echo "$ZONE"
+echo "$REGION"
+
+cluster_function() {
+  gcloud dataproc clusters create "$CLUSTER_NAME" \
+    --region "$REGION" \
+    --zone "$ZONE" \
+    --master-machine-type n1-standard-2 \
+    --worker-machine-type n1-standard-2 \
+    --num-workers 2 \
+    --worker-boot-disk-size 100 \
+    --worker-boot-disk-type pd-standard \
+    --no-address
+}
+
+cp_success=false
+
+while [ "$cp_success" = false ]; do
+  cluster_function
+  exit_status=$?
+
+  if [ "$exit_status" -eq 0 ]; then
+    echo ""
+    cp_success=true
+  else
+    echo ""
+
+    if gcloud dataproc clusters describe "$CLUSTER_NAME" --region "$REGION" &>/dev/null; then
+      echo ""
+      gcloud dataproc clusters delete "$CLUSTER_NAME" --region "$REGION" --quiet
+      echo "Cluster deleted. Retrying in 10 seconds..."
+    else
+      echo ""
+    fi
+    echo ""
+    sleep 10
+  fi
+done
+
+
+gcloud dataproc jobs submit spark \
+    --project $DEVSHELL_PROJECT_ID \
+    --region $REGION \
+    --cluster $CLUSTER_NAME \
+    --class org.apache.spark.examples.SparkPi \
+    --jars file:///usr/lib/spark/examples/jars/spark-examples.jar \
+    -- 1000
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+ 
