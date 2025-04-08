41a635b260464559db05b3ab7e0f403c48203302
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-08 19:14:46 +0530

    Update GSP457.sh

--- GSP/GSP457.sh
+++ GSP/GSP457.sh
@@ -8,53 +8,62 @@ pattern=(
 "**                                                      **"
 "**********************************************************"
 )
-gcloud services enable healthcare.googleapis.com
+gcloud services enable healthcare.googleapis.com --project=$PROJECT_ID
 
-sleep 20
+sleep 10
 
-gcloud pubsub topics create $TOPIC
+gcloud pubsub topics create $TOPIC --project=$PROJECT_ID
 
-bq --location=$LOCATION mk --dataset --description HCAPI-dataset $PROJECT_ID:$DATASET_ID
+bq --location=$LOCATION mk --dataset --description "HCAPI dataset" $PROJECT_ID:$DATASET_ID
 
-bq --location=$LOCATION mk --dataset --description HCAPI-dataset-de-id $PROJECT_ID:de_id
-
-gcloud projects add-iam-policy-binding $PROJECT_ID \
---member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
---role=roles/bigquery.dataEditor
-gcloud projects add-iam-policy-binding $PROJECT_ID \
---member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
---role=roles/bigquery.jobUser
+bq --location=$LOCATION mk --dataset --description "HCAPI dataset de-id" $PROJECT_ID:de_id
 
 gcloud healthcare datasets create $DATASET_ID \
---location=$LOCATION
+  --location=$LOCATION \
+  --project=$PROJECT_ID
+
+sleep 15
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com" \
+  --role="roles/bigquery.dataEditor"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com" \
+  --role="roles/bigquery.jobUser"
 
 gcloud healthcare fhir-stores create $FHIR_STORE_ID \
   --dataset=$DATASET_ID \
   --location=$LOCATION \
-  --version=R4
+  --version=R4 \
+  --project=$PROJECT_ID
 
 gcloud healthcare fhir-stores update $FHIR_STORE_ID \
   --dataset=$DATASET_ID \
   --location=$LOCATION \
-  --pubsub-topic=projects/$PROJECT_ID/topics/$TOPIC
+  --pubsub-topic=projects/$PROJECT_ID/topics/$TOPIC \
+  --project=$PROJECT_ID
 
 gcloud healthcare fhir-stores create de_id \
   --dataset=$DATASET_ID \
   --location=$LOCATION \
-  --version=R4
+  --version=R4 \
+  --project=$PROJECT_ID
 
 gcloud healthcare fhir-stores import gcs $FHIR_STORE_ID \
---dataset=$DATASET_ID \
---location=$LOCATION \
---gcs-uri=gs://spls/gsp457/fhir_devdays_gcp/fhir1/* \
---content-structure=BUNDLE_PRETTY
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --gcs-uri=gs://spls/gsp457/fhir_devdays_gcp/fhir1/* \
+  --content-structure=BUNDLE_PRETTY \
+  --project=$PROJECT_ID
 
 gcloud healthcare fhir-stores export bq $FHIR_STORE_ID \
---dataset=$DATASET_ID \
---location=$LOCATION \
---bq-dataset=bq://$PROJECT_ID.$DATASET_ID \
---schema-type=analytics
-
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --bq-dataset=bq://$PROJECT_ID.$DATASET_ID \
+  --schema-type=analytics \
+  --project=$PROJECT_ID
+  
 echo "COMPLETED MANUAL STEPS (y/n):"
 read -r answer
 if [[ $answer == "y" || $answer == "Y" ]]; then
