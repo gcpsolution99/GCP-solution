6c5b3c34757ae360833abcfd7c1e3e501403729b
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-08 19:01:40 +0530

    Create GSP457.sh

---
+++ GSP/GSP457.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+gcloud services enable healthcare.googleapis.com
+
+sleep 20
+
+gcloud pubsub topics create $TOPIC
+
+bq --location=$LOCATION mk --dataset --description HCAPI-dataset $PROJECT_ID:$DATASET_ID
+
+bq --location=$LOCATION mk --dataset --description HCAPI-dataset-de-id $PROJECT_ID:de_id
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/bigquery.dataEditor
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/bigquery.jobUser
+
+gcloud healthcare datasets create $DATASET_ID \
+--location=$LOCATION
+
+gcloud healthcare fhir-stores create $FHIR_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --version=R4
+
+gcloud healthcare fhir-stores update $FHIR_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --pubsub-topic=projects/$PROJECT_ID/topics/$TOPIC
+
+gcloud healthcare fhir-stores create de_id \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --version=R4
+
+gcloud healthcare fhir-stores import gcs $FHIR_STORE_ID \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--gcs-uri=gs://spls/gsp457/fhir_devdays_gcp/fhir1/* \
+--content-structure=BUNDLE_PRETTY
+
+gcloud healthcare fhir-stores export bq $FHIR_STORE_ID \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--bq-dataset=bq://$PROJECT_ID.$DATASET_ID \
+--schema-type=analytics
+
+echo "COMPLETED MANUAL STEPS (y/n):"
+read -r answer
+if [[ $answer == "y" || $answer == "Y" ]]; then
+    echo "SUBSCRIBE TO ABHI ARCADE SOLUTION....."
+else
+    "SUBSCRIBE TO ABHI ARCADE SOLUTION....."
+fi
+echo
+
+sleep 180
+
+gcloud healthcare fhir-stores export bq de_id \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--bq-dataset=bq://$PROJECT_ID.de_id \
+--schema-type=analytics
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+ 
