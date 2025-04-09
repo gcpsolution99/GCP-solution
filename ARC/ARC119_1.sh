2aab4c275b7d579cd5437167eb738ad6c04c28c2
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-09 19:31:00 +0530

    Create ARC119_1.sh

---
+++ ARC/ARC119_1.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+
+export REGION="${ZONE%-*}"
+export KEY_1=domain_type
+export VALUE_1=source_data
+
+gsutil mb -p $DEVSHELL_PROJECT_ID -l $REGION -b on gs://$DEVSHELL_PROJECT_ID-bucket/
+
+gcloud alpha dataplex lakes create customer-lake \
+    --display-name="Customer-Lake" \
+    --location=$REGION \
+    --labels="key_1=$KEY_1,value_1=$VALUE_1"
+
+gcloud dataplex zones create public-zone \
+    --lake=customer-lake \
+    --location=$REGION \
+    --type=RAW \
+    --resource-location-type=SINGLE_REGION \
+    --display-name="Public-Zone"
+
+gcloud dataplex environments create dataplex-lake-env \
+    --project=$DEVSHELL_PROJECT_ID \
+    --location=$REGION \
+    --lake=customer-lake \
+    --os-image-version=1.0 \
+    --compute-node-count 3 \
+    --compute-max-node-count 3
+
+gcloud data-catalog tag-templates create customer_data_tag_template \
+    --location=$REGION \
+    --display-name="Customer Data Tag Template" \
+    --field=id=data_owner,display-name="Data Owner",type=string,required=TRUE \
+    --field=id=pii_data,display-name="PII Data",type="enum(Yes|No)",required=TRUE
+
+echo "OPEN LINK: https://console.cloud.google.com/projectselector2/dataplex/groups"
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
