5af3b54e4820e3a61a5673b0ac0f4733529e0e48
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-09 19:09:47 +0530

    Create ARC119_4.sh

---
+++ ARC/ARC119_4.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export REGION="${ZONE%-*}"
+
+gcloud alpha dataplex lakes create customer-lake \
+--display-name="Customer-Lake" \
+ --location=$REGION \
+ --labels="key_1=$KEY_1,value_1=$VALUE_1"
+
+gcloud dataplex zones create public-zone \
+    --lake=customer-lake \
+    --location=$REGION \
+    --type=RAW \
+    --resource-location-type=SINGLE_REGION \
+    --display-name="Public-Zone"
+
+gcloud dataplex assets create customer-raw-data --location=$REGION \
+            --lake=customer-lake --zone=public-zone \
+            --resource-type=STORAGE_BUCKET \
+            --resource-name=projects/$DEVSHELL_PROJECT_ID/buckets/$DEVSHELL_PROJECT_ID-customer-bucket \
+            --discovery-enabled \
+            --display-name="Customer Raw Data"
+
+gcloud dataplex assets create customer-reference-data --location=$REGION \
+            --lake=customer-lake --zone=public-zone \
+            --resource-type=BIGQUERY_DATASET \
+            --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customer_reference_data \
+            --display-name="Customer Reference Data"
+
+echo "OPEN LINK: https://console.cloud.google.com/dataplex/lakes/customer-lake/zones/public-zone/create-entity;location=$REGION?project=$DEVSHELL_PROJECT_ID"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
