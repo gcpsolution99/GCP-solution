7714f6aa5bff446e58d3516c874ff187fb5ba9c3
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-09 20:41:33 +0530

    Create ARC119_3.sh

---
+++ ARC/ARC119_3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export REGION="${ZONE%-*}"
+bq mk --location=US Raw_data
+bq load --source_format=AVRO Raw_data.public-data gs://spls/gsp1145/users.avro
+
+gcloud dataplex zones create temperature-raw-data \
+    --lake=public-lake \
+    --location=$REGION \
+    --type=RAW \
+    --resource-location-type=SINGLE_REGION \
+    --display-name="temperature-raw-data"
+
+gcloud dataplex assets create customer-details-dataset \
+    --location=$REGION \
+    --lake=public-lake \
+    --zone=temperature-raw-data \
+    --resource-type=BIGQUERY_DATASET \
+    --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customer_reference_data \
+    --display-name="Customer Details Dataset" \
+    --discovery-enabled
+    
+gcloud data-catalog tag-templates create protected_data_template \
+    --location=$REGION \
+    --display-name="Protected Data Template" \
+    --field=id=protected_data_flag,display-name="Protected Data Flag",type='enum(Yes|No)',required=TRUE
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
