63c867d1bc0d305041c178ee925d03e06e90235b
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-10 19:58:56 +0530

    Update abhi.sh

--- Build a Multi-Modal GenAI Application: Challenge Lab/abhi.sh
+++ Build a Multi-Modal GenAI Application: Challenge Lab/abhi.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
 ID="$(gcloud projects list --format='value(PROJECT_ID)')"
 
 cat > GenerateImage.py <<EOF_END
@@ -7,44 +21,40 @@ import vertexai
 from vertexai.preview.vision_models import ImageGenerationModel
 
 def generate_image(
-    project_id: str, location: str, output_file: str, prompt: str
+  project_id: str, location: str, output_file: str, prompt: str
 ) -> vertexai.preview.vision_models.ImageGenerationResponse:
-    """Generate an image using a text prompt.
-    Args:
-      project_id: Google Cloud project ID, used to initialize Vertex AI.
-      location: Google Cloud region, used to initialize Vertex AI.
-      output_file: Local path to the output image file.
-      prompt: The text prompt describing what you want to see."""
+  """Generate an image using a text prompt.
+  Args:
+    project_id: Google Cloud project ID, used to initialize Vertex AI.
+    location: Google Cloud region, used to initialize Vertex AI.
+    output_file: Local path to the output image file.
+    prompt: The text prompt describing what you want to see."""
 
-    vertexai.init(project=project_id, location=location)
+  vertexai.init(project=project_id, location=location)
 
-    model = ImageGenerationModel.from_pretrained("imagegeneration@002")
+  model = ImageGenerationModel.from_pretrained("imagen-3.0-generate-002")
 
-    images = model.generate_images(
-        prompt=prompt,
-        # Optional parameters
-        number_of_images=1,
-        seed=1,
-        add_watermark=False,
-    )
+  images = model.generate_images(
+    prompt=prompt,
+    # Optional parameters
+    number_of_images=1,
+    seed=1,
+    add_watermark=False,
+  )
 
-    images[0].save(location=output_file)
+  images[0].save(location=output_file)
 
-    return images
+  return images
 
 generate_image(
-    project_id='$ID',
-    location='$REGION',
-    output_file='image.jpeg',
-    prompt='Create an image of a cricket ground in the heart of Los Angeles',
-    )
-
+  project_id='$ID',
+  location='$REGION',
+  output_file='image.jpeg',
+  prompt='Create an image of a cricket ground in the heart of Los Angeles',
+)
 EOF_END
 
 /usr/bin/python3 /home/student/GenerateImage.py
-
-
-
 
 ID="$(gcloud projects list --format='value(PROJECT_ID)')"
 
@@ -52,36 +62,30 @@ cat > genai.py <<EOF_END
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part
 
-
 def generate_text(project_id: str, location: str) -> str:
-    # Initialize Vertex AI
-    vertexai.init(project=project_id, location=location)
-    # Load the model
-    multimodal_model = GenerativeModel("gemini-1.0-pro-vision")
-    # Query the model
-    response = multimodal_model.generate_content(
-        [
-            # Add an example image
-            Part.from_uri(
-                "gs://generativeai-downloads/images/scones.jpg", mime_type="image/jpeg"
-            ),
-            # Add an example query
-            "what is shown in this image?",
-        ]
-    )
+  # Initialize Vertex AI
+  vertexai.init(project=project_id, location=location)
+  # Load the model
+  multimodal_model = GenerativeModel("gemini-2.0-flash-001")
+  # Query the model
+  response = multimodal_model.generate_content(
+    [
+      # Add an example image
+      Part.from_uri(
+        "gs://generativeai-downloads/images/scones.jpg", mime_type="image/jpeg"
+      ),
+      # Add an example query
+      "what is shown in this image?",
+    ]
+  )
 
-    return response.text
-
-# --------  Important: Variable declaration  --------
+  return response.text
 
 project_id = "$ID"
 location = "$REGION"
 
-#  --------   Call the Function  --------
-
 response = generate_text(project_id, location)
 print(response)
-
 EOF_END
 
 /usr/bin/python3 /home/student/genai.py
@@ -89,3 +93,15 @@ EOF_END
 sleep 30
 
 /usr/bin/python3 /home/student/genai.py
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
