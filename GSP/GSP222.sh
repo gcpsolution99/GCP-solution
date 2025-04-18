88e280325fd914eba7b4e6c91b1419ed4cb1ba90
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-04-18 15:14:08 +0530

    Update GSP222.sh

--- GSP/GSP222.sh
+++ GSP/GSP222.sh
@@ -1,5 +1,30 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
 export PROJECT_ID=$(gcloud config get-value project)
 gcloud services enable texttospeech.googleapis.com
 gcloud iam service-accounts create tts-qwiklab
 gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$PROJECT_ID.iam.gserviceaccount.com
 export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
