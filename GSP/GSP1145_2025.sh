230f01145a175f4cbd1a9cb6ec6ea38b1a721d38
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-05-06 19:58:17 +0530

    Update GSP1145_2025.sh

--- GSP/GSP1145_2025.sh
+++ GSP/GSP1145_2025.sh
@@ -12,33 +12,31 @@ for line in "${pattern[@]}"
 do
     echo -e "${YELLOW}${line}${NC}"
 done
-gcloud services enable dataplex.googleapis.com
-gcloud services enable datacatalog.googleapis.com
-gcloud dataplex lakes create orders-lake \
-  --location=$REGION \
-  --display-name="Orders Lake"
+gcloud auth list
 
-gcloud dataplex zones create customer-curated-zone \
-    --location=$REGION \
-    --lake=orders-lake \
-    --display-name="Customer Curated Zone" \
-    --resource-location-type=SINGLE_REGION \
-    --type=CURATED \
-    --discovery-enabled \
-    --discovery-schedule="0 * * * *"
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
-gcloud dataplex assets create customer-details-dataset \
---location=$REGION \
---lake=orders-lake \
---zone=customer-curated-zone \
---display-name="Customer Details Dataset" \
---resource-type=BIGQUERY_DATASET \
---resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customers \
---discovery-enabled
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
-gcloud data-catalog tag-templates create protected_data_template --location=$REGION --field=id=protected_data_flag,display-name="Protected Data Flag",type='enum(YES|NO)' --display-name="Protected Data Template"
+export PROJECT_ID=$(gcloud config get-value project)
 
-echo "Open dataplex: ""https://console.cloud.google.com/dataplex/search?project=$DEVSHELL_PROJECT_ID&qSystems=DATAPLEX"
+gcloud config set compute/zone "$ZONE"
+
+gcloud config set compute/region "$REGION"
+
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+gcloud services enable dataplex.googleapis.com datacatalog.googleapis.com --project=$DEVSHELL_PROJECT_ID
+
+sleep 10
+
+gcloud dataplex lakes create orders-lake --location=$REGION --display-name="Orders Lake"
+
+gcloud dataplex zones create customer-curated-zone --location=$REGION --display-name="Customer Curated Zone" --lake=orders-lake --resource-location-type=SINGLE_REGION --type=CURATED --discovery-enabled --discovery-schedule="0 * * * *"
+
+gcloud dataplex assets create customer-details-dataset --location=$REGION --display-name="Customer Details Dataset" --lake=orders-lake --zone=customer-curated-zone --resource-type=BIGQUERY_DATASET --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customers --discovery-enabled
+
+gcloud data-catalog tag-templates create protected_data_template --location=$REGION --display-name="Protected Data Template" --field=id=protected_data_flag,display-name="Protected Data Flag",type='enum(YES|NO)'
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
