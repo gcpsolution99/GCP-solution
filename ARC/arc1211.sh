0826b3b213c682ddd61998a12aee14b4d9b9e734
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-05-07 19:47:50 +0530

    Create arc1211.sh

---
+++ ARC/arc1211.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export REGION=$(gcloud compute project-info describe \
+ --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+ 
+ gcloud services enable artifactregistry.googleapis.com
+ 
+ gcloud artifacts repositories create container-registry \
+  --repository-format=docker \
+  --location=$REGION \
+  --description="Docker registry with cleanup policy"
+ 
+ gcloud artifacts repositories create apt-registry \
+  --repository-format=apt \
+  --location=$REGION \
+  --description="APT registry with cleanup policy"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
