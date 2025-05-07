97d84295b65e22f76f78ee7107a0783f83d4727d
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-05-07 17:33:30 +0530

    Create ARC1223.sh

---
+++ ARC/ARC1223.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export PROJECT_ID=$(gcloud config get-value project)
+echo "Project ID set to: ${PROJECT_ID}"
+echo
+
+echo
+read -p "Enter your region: " REGION
+echo
+read -p "Enter Cloud Function name: " FUNCTION_NAME
+echo
+
+cat > cloud-function/index.js <<EOF
+exports.helloWorld = (req, res) => {
+  res.send('Hello from Cloud Function!');
+};
+EOF
+
+cat > cloud-function/package.json <<EOF
+{
+  "name": "cf-nodejs",
+  "version": "1.0.0",
+  "main": "index.js"
+}
+EOF
+
+gcloud functions deploy ${FUNCTION_NAME} \
+  --gen2 \
+  --runtime=nodejs20 \
+  --region=${REGION} \
+  --source=cloud-function \
+  --entry-point=helloWorld \
+  --trigger-http \
+  --max-instances=5 \
+  --allow-unauthenticated
+
+echo -e "\n"
+cd ~
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
