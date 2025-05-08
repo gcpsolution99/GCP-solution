b2dbae1ef801e14844b58d26f1cc7594cfe762ce
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-05-08 20:09:00 +0530

    Create arc1208.sh

---
+++ ARC/arc1208.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+read -p "Export REGION :" REGION
+
+REPO_NAME="container-registry"
+FORMAT="DOCKER"
+POLICY_NAME="Grandfather"
+KEEP_COUNT=3
+
+gcloud artifacts repositories create $REPO_NAME \
+  --repository-format=$FORMAT \
+  --location=$REGION \
+  --description="Docker repo for container images"
+  
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
