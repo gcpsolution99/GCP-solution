e1daa6a84041cb12de3d167f8433dd8a1cfbadbc
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-06-20 21:14:41 +0530

    Create GSP750.sh

---
+++ GSP/GSP750.sh
@@ -0,0 +1,294 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud auth list
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export PROJECT_ID=$(gcloud config get-value project)
+gcloud config set project $DEVSHELL_PROJECT_ID
+gcloud config set compute/zone "$ZONE"
+gcloud config set compute/region "$REGION"
+
+echo 'terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "'"$PROJECT_ID"'"
+  region  = "$REGION"
+  zone    = "$ZONE"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}' > main.tf
+
+terraform init
+terraform apply -auto-approve
+
+echo 'terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "'"$PROJECT_ID"'"
+  region  = "'"$REGION"'"
+  zone    = "'"$ZONE"'"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}' > main.tf
+
+terraform apply -auto-approve
+
+echo 'terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "'"$PROJECT_ID"'"
+  region  = "'"$REGION"'"
+  zone    = "'"$ZONE"'"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags        = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}' > main.tf
+
+terraform apply -auto-approve
+
+echo 'terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "'"$PROJECT_ID"'"
+  region  = "'"$REGION"'"
+  zone    = "'"$ZONE"'"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags        = ["web", "dev"]
+      boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}' > main.tf
+
+terraform apply -auto-approve
+terraform destroy -auto-approve
+terraform apply -auto-approve
+
+echo 'terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "'"$PROJECT_ID"'"
+  region  = "'"$REGION"'"
+  zone    = "'"$ZONE"'"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags        = ["web", "dev"]
+      boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}' > main.tf
+
+terraform plan
+
+echo 'terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "'"$PROJECT_ID"'"
+  region  = "'"$REGION"'"
+  zone    = "'"$ZONE"'"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags        = ["web", "dev"]
+      boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+    network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}' > main.tf
+
+terraform plan -out static_ip
+terraform apply "static_ip"
+
+echo 'terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "'"$PROJECT_ID"'"
+  region  = "'"$REGION"'"
+  zone    = "'"$ZONE"'"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags        = ["web", "dev"]
+      boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+    network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+resource "google_storage_bucket" "example_bucket" {
+  name     = "'"$PROJECT_ID"'"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  depends_on = [google_storage_bucket.example_bucket]
+  name         = "terraform-instance-2"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}' > main.tf
+
+terraform plan
+terraform apply -auto-approve
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
