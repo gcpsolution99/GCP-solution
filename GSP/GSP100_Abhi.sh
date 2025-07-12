ec1adad3f7a02dca9493304980cc58b751b61610
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-07-12 12:40:03 +0530

    Create GSP100_Abhi.sh

---
+++ GSP/GSP100_Abhi.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud auth list
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+gcloud config set compute/zone "$ZONE"
+gcloud config set compute/region "$REGION"
+gcloud container clusters create --machine-type=e2-medium --zone=$ZONE lab-cluster
+gcloud container clusters get-credentials lab-cluster
+kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
+kubectl expose deployment hello-server --type=LoadBalancer --port 8080
+gcloud container clusters delete lab-cluster
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
