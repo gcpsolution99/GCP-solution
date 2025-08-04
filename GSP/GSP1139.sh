9471690dccd79e4e6c5e1b6c144dfe5ec13878dd
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-04 21:34:51 +0530

    Create GSP1139.sh

---
+++ GSP/GSP1139.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud auth list
+
+gcloud services enable documentai.googleapis.com
+pip3 install --upgrade pandas
+pip3 install --upgrade google-cloud-documentai
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
