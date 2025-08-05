59cf8115d55db89e9c25cfe9c6f694ad47761582
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-05 18:14:38 +0530

    Create Cost_Optimization.sh

---
+++ GSP/Cost_Optimization.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud auth list
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export PROJECT_ID=$(gcloud config get-value project)
+gcloud config set compute/zone "$ZONE"
+gcloud compute instances stop lab-vm --zone="$ZONE"
+sleep 15
+gcloud compute instances set-machine-type lab-vm --machine-type e2-medium --zone="$ZONE"
+sleep 16
+gcloud compute instances start lab-vm  --zone="$ZONE"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
