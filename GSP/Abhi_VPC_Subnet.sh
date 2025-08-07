c638d21cc65371526cbb6289bea825cf93b725f5
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-07 20:15:38 +0530

    Create Abhi_VPC_Subnet.sh

---
+++ GSP/Abhi_VPC_Subnet.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+gcloud storage buckets create gs://$PROJECT_ID-tf-state --project=$PROJECT_ID --location=$REGION --uniform-bucket-level-access
+
+gcloud services enable cloudresourcemanager.googleapis.com --project=$PROJECT_ID
+
+
+cat > firewall.tf <<EOF_END
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "$PROJECT_ID-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = "$PROJECT_ID"
+  region  = "$REGION"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "custom-vpc-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet_us" {
+  name            = "subnet-us"
+  ip_cidr_range   = "10.10.1.0/24"
+  region          = "$REGION"
+  network         = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "allow-icmp"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+EOF_END
+
+
+cat > variables.tf <<EOF_END
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project"
+  default     = "$PROJECT_ID"
+}
+
+variable "region" {
+  type = string
+  description = "The region to deploy resources in"
+  default = "$REGION"
+}
+EOF_END
+
+
+cat > outputs.tf <<EOF_END
+output "network_name" {
+  value       = google_compute_network.vpc_network.name
+  description = "The name of the VPC network"
+}
+
+output "subnet_name" {
+  value       = google_compute_subnetwork.subnet_us.name
+  description = "The name of the subnetwork"
+}
+EOF_END
+
+terraform init
+
+terraform plan
+
+terraform apply --auto-approve
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
