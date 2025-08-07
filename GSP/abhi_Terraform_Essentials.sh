106e9cda7ee5a3a63b0c549732237746f3f4ffc1
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-07 18:49:33 +0530

    Create abhi_Terraform_Essentials.sh

---
+++ GSP/abhi_Terraform_Essentials.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set project $PROJECT_ID
+gsutil mb -l $REGION gs://$PROJECT_ID-tf-state
+
+
+cat > main.tf <<EOF_END
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "$PROJECT_ID-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "default" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = "default"
+
+    access_config {
+    }
+  }
+}
+EOF_END
+
+
+
+cat > variables.tf <<EOF_END
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project"
+  default = "$PROJECT_ID"
+}
+
+variable "region" {
+  type        = string
+  description = "The region to deploy resources in"
+  default     = "$REGION"
+}
+
+variable "zone" {
+  type        = string
+  description = "The zone to deploy resources in"
+  default     = "$ZONE"
+}
+EOF_END
+
+terraform init
+
+terraform plan
+
+terraform apply --auto-approve
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
