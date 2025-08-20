f10abd23b29b26b87a9739ec96449df3ef2fe8db
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-20 19:29:23 +0530

    Create GSP233_abhi.sh

---
+++ GSP/GSP233_abhi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud auth list
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+gcloud config set compute/zone "$ZONE"
+gcloud config set compute/region "$REGION"
+gsutil -m cp -r gs://spls/gsp233/* .
+cd tf-gke-k8s-service-lb
+terraform init
+terraform apply -var="region=$REGION" -var="location=$ZONE" --auto-approve
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
