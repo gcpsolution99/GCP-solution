4585147e6603670b56b0274e011436003e557ca6
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-20 19:53:37 +0530

    Create GSP234_abhi.sh

---
+++ GSP/GSP234_abhi.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+mkdir sql-with-terraform
+cd sql-with-terraform
+gsutil cp -r gs://spls/gsp234/gsp234.zip .
+unzip gsp234.zip
+terraform init
+terraform plan -out=tfplan
+terraform apply tfplan
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
