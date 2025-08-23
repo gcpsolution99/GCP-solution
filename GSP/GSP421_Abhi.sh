6086cc3c9d2f94040e06f9ab23d5722f3c8db3f6
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-23 19:41:16 +0530

    Create GSP421_Abhi.sh

---
+++ GSP/GSP421_Abhi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+gsutil mb gs://$DEVSHELL_PROJECT_ID-2
+gsutil cp demo-image1.png gs://$DEVSHELL_PROJECT_ID/demo-image1.png
+gsutil cp demo-image2.png gs://$DEVSHELL_PROJECT_ID/demo-image2.png
+gsutil cp demo-image1-copy.png gs://$DEVSHELL_PROJECT_ID-2/demo-image1-copy.png
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
