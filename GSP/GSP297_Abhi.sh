bcf995026294bb43137ee50379da0ccd85028bab
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-25 18:58:41 +0530

    Create GSP297_Abhi.sh

---
+++ GSP/GSP297_Abhi.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+export BUCKET=$(gcloud config get-value project)
+gsutil mb "gs://$BUCKET"
+sleep 10
+gsutil retention set 10s "gs://$BUCKET"
+gsutil retention get "gs://$BUCKET"
+gsutil cp gs://spls/gsp297/dummy_transactions "gs://$BUCKET/"
+gsutil ls -L "gs://$BUCKET/dummy_transactions"
+sleep 10
+gsutil retention lock "gs://$BUCKET/"
+gsutil retention temp set "gs://$BUCKET/dummy_transactions"
+gsutil rm "gs://$BUCKET/dummy_transactions"
+gsutil retention temp release "gs://$BUCKET/dummy_transactions"
+gsutil retention event-default set "gs://$BUCKET/"
+gsutil cp gs://spls/gsp297/dummy_loan "gs://$BUCKET/"
+gsutil ls -L "gs://$BUCKET/dummy_loan"
+gsutil retention event release "gs://$BUCKET/dummy_loan"
+gsutil ls -L "gs://$BUCKET/dummy_loan"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
