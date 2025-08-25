91e8810e14062dbed635ff66df7ba7d2f25101c3
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-08-25 18:12:21 +0530

    Update GSP421_Abhi.sh

--- GSP/GSP421_Abhi.sh
+++ GSP/GSP421_Abhi.sh
@@ -15,8 +15,8 @@ done
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 gsutil mb gs://$DEVSHELL_PROJECT_ID-2
 curl -LO raw.githubusercontent.com/gcpsolution99/GCP-solution/refs/heads/main/demo-image1.png
-curl -LO raw.githubusercontent.com/gcpsolution99/GCP-solution/refs/heads/main/demo-image1-copy.png
 curl -LO raw.githubusercontent.com/gcpsolution99/GCP-solution/refs/heads/main/demo-image2.png
+curl -LO raw.githubusercontent.com/gcpsolution99/GCP-solution/refs/heads/main/demo-image1-copy.png
 gsutil cp demo-image1.png gs://$DEVSHELL_PROJECT_ID/demo-image1.png
 gsutil cp demo-image2.png gs://$DEVSHELL_PROJECT_ID/demo-image2.png
 gsutil cp demo-image1-copy.png gs://$DEVSHELL_PROJECT_ID-2/demo-image1-copy.png
