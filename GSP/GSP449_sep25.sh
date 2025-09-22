2c48186e115991cbefe1245c7f9ee88c11d69888
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-09-22 22:00:36 +0530

    Create GSP449_sep25.sh

---
+++ GSP/GSP449_sep25.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+gcloud auth list
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export PROJECT_ID=$(gcloud config get-value project)
+gcloud config set compute/zone "$ZONE"
+gcloud config set compute/region "$REGION"
+
+gsutil cp gs://spls/gsp449/gke-cloud-sql-postgres-demo.tar.gz .
+tar -xzvf gke-cloud-sql-postgres-demo.tar.gz
+
+cd gke-cloud-sql-postgres-demo
+
+PG_EMAIL=$(gcloud config get-value account)
+
+./create.sh dbadmin $PG_EMAIL
+
+
+sleep 10
+
+POD_ID=$(kubectl --namespace default get pods -o name | cut -d '/' -f 2)
+
+kubectl expose pod $POD_ID --port=80 --type=LoadBalancer
+
+kubectl get svc
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
