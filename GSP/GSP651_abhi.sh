3565753998d3222a48e2440ea2438738b6cab089
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2025-10-22 11:49:39 +0530

    Refactor GSP651_abhi25.sh for BigQuery quota management
    Removed multiple gcloud compute instance creation commands and replaced them with quota management commands for BigQuery.

---
+++ GSP/GSP651_abhi.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+bq query --use_legacy_sql=false \
+"
+SELECT
+    w1mpro_ep,
+    mjd,
+    load_id,
+    frame_id
+FROM
+    \`bigquery-public-data.wise_all_sky_data_release.mep_wise\`
+ORDER BY
+    mjd ASC
+LIMIT 500
+"
+
+gcloud alpha services quota list --service=bigquery.googleapis.com --consumer=projects/${DEVSHELL_PROJECT_ID} --filter="usage"
+
+gcloud alpha services quota update --consumer=projects/${DEVSHELL_PROJECT_ID} --service bigquery.googleapis.com --metric bigquery.googleapis.com/quota/query/usage --value 262144 --unit 1/d/{project}/{user} --force
+
+gcloud alpha services quota list --service=bigquery.googleapis.com --consumer=projects/${DEVSHELL_PROJECT_ID} --filter="usage"
+
+bq query --use_legacy_sql=false --nouse_cache \
+"
+ SELECT
+     w1mpro_ep,
+     mjd,
+     load_id,
+     frame_id
+ FROM
+     \`bigquery-public-data.wise_all_sky_data_release.mep_wise\`
+ ORDER BY
+     mjd ASC
+ LIMIT 500
+"
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
